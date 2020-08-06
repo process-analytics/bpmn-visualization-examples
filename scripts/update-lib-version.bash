7bc80d68b82d5b9d620d3e0ed6b9bec2077ba439
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-08-06 17:29:17 +0200

    chore(update): provide a script to update examples on new lib release
    Tested by running it from the 'root', 'scripts' and 'examples/demo' folders.

---
+++ scripts/update-lib-version.bash
@@ -0,0 +1,24 @@
+#! /bin/bash
+set -e
+
+############################################################
+# update all html files in the examples folder
+############################################################
+if [ $# -ne 1 ]; then
+	echo "Mandatory parameter <new_version> missing."
+	exit 1
+fi
+NEW_VERSION=$1
+echo "Updating examples to make them use demo@$NEW_VERSION"
+
+SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+EXAMPLES_DIRECTORY="$SCRIPT_DIRECTORY/../examples"
+pushd "$EXAMPLES_DIRECTORY" > /dev/null
+
+echo "Search for files in $(pwd)"
+# lines to update start with something like "<script src="../../demo/0.1.6/"
+sed -i -E "s#<script src=\"../../demo/[^/]+/#<script src=\"../../demo/$NEW_VERSION/#" **/*.{html,md}
+
+echo "Files updated"
+popd > /dev/null
+pwd
