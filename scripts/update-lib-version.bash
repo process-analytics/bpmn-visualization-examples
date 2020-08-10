e51620286f514985577c026866e4efe036d48481
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-08-10 18:06:40 +0200

    specific sed command on macOS

--- scripts/update-lib-version.bash
+++ scripts/update-lib-version.bash
@@ -17,7 +17,11 @@ pushd "$EXAMPLES_DIRECTORY" > /dev/null
 
 echo "Search for files in $(pwd)"
 # lines to update start with something like "<script src="../../demo/0.1.6/"
-sed -i -E "s#<script src=\"../../demo/[^/]+/#<script src=\"../../demo/$NEW_VERSION/#" **/*.{html,md}
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	sed -i '' -E "s#<script src=\"../../demo/[^/]+/#<script src=\"../../demo/$NEW_VERSION/#" **/*.{html,md}
+else
+	sed -i -E "s#<script src=\"../../demo/[^/]+/#<script src=\"../../demo/$NEW_VERSION/#" **/*.{html,md}
+fi
 
 echo "Files updated"
 popd > /dev/null
