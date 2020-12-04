96a751ce95ea107484f6682e633731bf5c9c9c8f
Marcin Michniewicz <45601541+aibcmars@users.noreply.github.com>
2020-12-04 17:12:36 +0100

    [EXAMPLE] cleaned and refactored added detach popover and some improvements

--- examples/custom-navigation/popover/index.js
+++ examples/custom-navigation/popover/index.js
@@ -1,6 +1,6 @@
 // TODO duplicated with the 'diagram-fit-variants' example
 function getBpmnDiagramHorizontal() {
-    return `<?xml version="1.0" encoding="UTF-8"?>
+  return `<?xml version="1.0" encoding="UTF-8"?>
         <bpmn:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" id="Definitions_1jzeku5" targetNamespace="http://example.com/schema/bpmn">
           <bpmn:process id="Process_1tm11eu" isExecutable="false">
             <bpmn:startEvent id="StartEvent_07yal9c" name="Start">
@@ -11,15 +11,15 @@ function getBpmnDiagramHorizontal() {
               <bpmn:outgoing>Flow_0wu2yg6</bpmn:outgoing>
             </bpmn:task>
             <bpmn:sequenceFlow id="Flow_0alm7tv" sourceRef="StartEvent_07yal9c" targetRef="Activity_1xr04un" />
-            <bpmn:sequenceFlow id="Flow_0wu2yg6" sourceRef="Activity_1xr04un" targetRef="Activity_0kn4d46" />
-            <bpmn:sequenceFlow id="Flow_1gcjvke" sourceRef="Activity_0kn4d46" targetRef="Activity_1j15wcw" />
+            <bpmn:sequenceFlow id="Flow_0wu2yg6" sourceRef="Activity_1xr04un" targetRef="Human_Task_2" />
+            <bpmn:sequenceFlow id="Flow_1gcjvke" sourceRef="Human_Task_2" targetRef="Activity_1j15wcw" />
             <bpmn:sequenceFlow id="Flow_1bmck4z" sourceRef="Activity_1j15wcw" targetRef="Activity_0wbisol" />
             <bpmn:sequenceFlow id="Flow_1i7r7y7" sourceRef="Activity_0wbisol" targetRef="Activity_0y3sd80" />
             <bpmn:sequenceFlow id="Flow_12yysoe" sourceRef="Activity_0y3sd80" targetRef="Event_157byoi" />
             <bpmn:endEvent id="Event_157byoi" name="End">
               <bpmn:incoming>Flow_12yysoe</bpmn:incoming>
             </bpmn:endEvent>
-            <bpmn:userTask id="Activity_0kn4d46" name="Task 2">
+            <bpmn:userTask id="Human_Task_2" name="Task 2">
               <bpmn:incoming>Flow_0wu2yg6</bpmn:incoming>
               <bpmn:outgoing>Flow_1gcjvke</bpmn:outgoing>
             </bpmn:userTask>
@@ -71,7 +71,7 @@ function getBpmnDiagramHorizontal() {
               <bpmndi:BPMNShape id="Activity_1xr04un_di" bpmnElement="Activity_1xr04un">
                 <dc:Bounds x="250" y="59" width="100" height="80" />
               </bpmndi:BPMNShape>
-              <bpmndi:BPMNShape id="Activity_03oln2l_di" bpmnElement="Activity_0kn4d46">
+              <bpmndi:BPMNShape id="Activity_03oln2l_di" bpmnElement="Human_Task_2">
                 <dc:Bounds x="410" y="59" width="100" height="80" />
               </bpmndi:BPMNShape>
               <bpmndi:BPMNShape id="Activity_0owvwg0_di" bpmnElement="Activity_1j15wcw">
@@ -95,57 +95,102 @@ function getBpmnDiagramHorizontal() {
         `;
 }
 
+// Popover settings
+// The class is also used in style where CSS rules applies - it is not obligatory but serves here to differentiate the task from the other
+// .chartreuse rect{
+//   fill: chartreuse;
+// }
+const popoverForeignObjectClass = 'chartreuse';
+
 const bpmnContainerId = 'bpmn-container';
 const bpmnContainerElt = window.document.getElementById(bpmnContainerId);
-const bpmnVisualization = new bpmnvisu.BpmnVisualization(bpmnContainerElt, { mouseNavigationSupport: true });
-const diagram = getBpmnDiagramHorizontal();
+const bpmnVisualization = new bpmnvisu.BpmnVisualization(bpmnContainerElt);
+bpmnVisualization.load(getBpmnDiagramHorizontal(), {fit: {type: 'Center', margin: 50}});
 
-bpmnVisualization.load(diagram, { fit: {type: 'Center'} });
+const searchElt = document.getElementById('search-id');
 
+// Handle the availability to attach popover based on current element state
+// This example allows only one popover at a time
+document.getElementById('search-id').oninput = function (e) {
+  const shapeElt = bpmnVisualization.htmlElementRegistry.getBpmnHtmlElement(e.target.value);
+  if (!shapeElt) {
+    return;
+  }
+  if (shapeElt.classList.contains(popoverForeignObjectClass)) {
+    disablePopoverAttachment();
+  } else {
+    enablePopoverAttachment();
+  }
+}
+
+// Attach popover for an element found by id from search input
 document.getElementById('attach-popover').onclick = function () {
-    const searchElt = document.getElementById('search-id');
-    const popoverContent = document.getElementById('popover-content');
+  const shapeElt = bpmnVisualization.htmlElementRegistry.getBpmnHtmlElement(searchElt.value);
+  shapeElt.classList.add(popoverForeignObjectClass);
+  const foreignObjectWrapperElt = createForeignObjectContainer(shapeElt);
+  const popoverDivElt = createPopoverDiv();
+  foreignObjectWrapperElt.prepend(popoverDivElt);
+  shapeElt.append(foreignObjectWrapperElt);
+  setPopoverStyle(popoverDivElt);
+  disablePopoverAttachment();
+}
 
-    const shapeElt = bpmnVisualization.htmlElementRegistry.getBpmnHtmlElement(searchElt.value);
-    shapeElt.classList.add('chartreuse');
-    const rect = shapeElt.getElementsByTagName('rect')[0];
+// Detach popover for an element found by id from search input if it exists
+document.getElementById('detach-popover').onclick = function () {
+  const shapeElt = bpmnVisualization.htmlElementRegistry.getBpmnHtmlElement(searchElt.value);
+  if (!shapeElt) {
+    return;
+  }
+  if (shapeElt.classList.contains(popoverForeignObjectClass)) {
+    shapeElt.classList.remove(popoverForeignObjectClass);
+    shapeElt.removeChild(shapeElt.lastChild);
+    enablePopoverAttachment();
+  }
+}
 
-    const foreign = document.createElementNS('http://www.w3.org/2000/svg',"foreignObject");
-    foreign.setAttribute('width', '100%');
-    foreign.setAttribute('height','100%');
-    foreign.setAttribute('x',  rect.getAttribute('x'));
-    foreign.setAttribute('y', rect.getAttribute('y'));
-    foreign.setAttribute('width', rect.getAttribute('width'));
-    foreign.setAttribute('height', rect.getAttribute('height'));
-    foreign.setAttribute('style', 'overflow: visible;');
-    foreign.classList.add('bpmn-popover-foreign');
+function createPopoverDiv() {
+  const popoverDivElt = document.createElement('div');
+  const popoverContent = document.getElementById('popover-content');
+  popoverDivElt.innerHTML = popoverContent.value;
+  return popoverDivElt;
+}
 
-    const popoverContainer = document.createElement('div');
-    popoverContainer.classList.add('hidden');
-    popoverContainer.classList.add('bpmn-popover');
-    //TODO: think about sanitizing raw HTML (or leave it for the integration but explain)
-    popoverContainer.innerHTML = popoverContent.value;
-    foreign.prepend(popoverContainer);
-    shapeElt.append(foreign);
-    // setTimeout(() => {
+function createForeignObjectContainer(shapeElt) {
+  const rectElt = shapeElt.getElementsByTagName('rect')[0];
+  const foreignObjectElt = document.createElementNS('http://www.w3.org/2000/svg', "foreignObject");
+  foreignObjectElt.setAttribute('width', '100%');
+  foreignObjectElt.setAttribute('height', '100%');
+  foreignObjectElt.setAttribute('x', rectElt.getAttribute('x'));
+  foreignObjectElt.setAttribute('y', rectElt.getAttribute('y'));
+  foreignObjectElt.setAttribute('width', rectElt.getAttribute('width'));
+  foreignObjectElt.setAttribute('height', rectElt.getAttribute('height'));
+  foreignObjectElt.setAttribute('style', 'overflow: visible;');
+  foreignObjectElt.classList.add('bpmn-popover-foreign');
+  return foreignObjectElt;
+}
 
-    popoverContainer.setAttribute('style', 'position: absolute;');
-    const boundingRect = popoverContainer.getBoundingClientRect();
-    console.log(boundingRect);
-    const popoverHeight = boundingRect.height;
-    // speech bubble indicator height is 21
-    const style = `position: absolute; top: -${popoverHeight + 21}px`;
-    console.log(style);
-    popoverContainer.setAttribute('style', style);
-    popoverContainer.classList.remove('hidden');
-    // }, 100)
+function setPopoverStyle(popoverContainer) {
+  popoverContainer.classList.add('hidden');
+  popoverContainer.classList.add('bpmn-popover');
+  popoverContainer.setAttribute('style', 'position: absolute;');
+  const boundingRect = popoverContainer.getBoundingClientRect();
+  const popoverHeight = boundingRect.height;
+  // speech bubble indicator height is 21
+  const style = `position: absolute; top: -${popoverHeight + 21}px`;
+  popoverContainer.setAttribute('style', style);
+  popoverContainer.classList.remove('hidden');
+}
 
-    foreign.addEventListener('mouseenter', e => {
-        console.log('mouseenter', e);
-    });
+function setButtonState(btnId, disabled) {
+  document.getElementById(btnId).disabled = disabled;
+}
 
-    foreign.addEventListener('mouseleave', e => {
-        console.log('mouseleave', e);
-    })
+function disablePopoverAttachment() {
+  setButtonState('attach-popover', true);
+  setButtonState('detach-popover', false);
+}
 
+function enablePopoverAttachment() {
+  setButtonState('attach-popover', false);
+  setButtonState('detach-popover', true);
 }
