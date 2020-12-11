e4f304cf3ce233adf71d68b44355c762d2aabcff
Marcin Michniewicz <45601541+aibcmars@users.noreply.github.com>
2020-12-11 14:24:04 +0100

    [EXAMPLE] Popover - static (#105)
    * dynamic positioning depending on the shape element
    * customizable content
    
    
    Co-authored-by: Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
    Co-authored-by: Souchet Céline <4921914+csouchet@users.noreply.github.com>

---
+++ examples/custom-interaction/popover-static/README.md
@@ -0,0 +1,19 @@
+# Add a popover with information to the task found by it's id
+
+Javascript example to demonstrate how to open a new BPMN diagram in a tab.
+- [__:fast_forward: live environment__](https://cdn.statically.io/gh/process-analytics/bpmn-visualization-examples/master/examples/custom-interaction/popover/index.html)
+- to run locally, open the [index.html](index.html) directly in a Web Browser
+
+## ♻️ Usage
+
+After the `BpmnVisualization` instantiation, get the HTML element corresponding to the id from search input.
+
+⚠️  In order to avoid having to many content in the README, we simplify it. You can find all the content of the example in [index.js](index.js).
+What is important to know:
+- Definition of style for the popover are in index.html
+- The HTML passed as the content of the popover is not sanitized - it is up to integrator to do so.
+
+
+⚠️ It's not possible to use BPMN diagram navigation at the moment. \
+Panning and zooming causes the diagram to be redrawn.
+A new API comes later to support this case.
---
+++ examples/custom-interaction/popover-static/index.js
@@ -0,0 +1,90 @@
+const popoverForeignObjectClass = 'chartreuse';
+
+const bpmnContainerId = 'bpmn-container';
+const bpmnContainerElt = window.document.getElementById(bpmnContainerId);
+const bpmnVisualization = new bpmnvisu.BpmnVisualization(bpmnContainerElt);
+bpmnVisualization.load(getHorizontalBpmnDiagram(), {fit: {type: 'Center', margin: 50}});
+
+const searchElt = document.getElementById('search-id');
+
+// Handle the availability to attach popover based on current element state
+// This example allows only one popover at a time
+document.getElementById('search-id').oninput = function (e) {
+  const shapeElt = bpmnVisualization.htmlElementRegistry.getBpmnHtmlElement(e.target.value);
+  if (!shapeElt) {
+    return;
+  }
+  if (shapeElt.classList.contains(popoverForeignObjectClass)) {
+    disablePopoverAttachment();
+  } else {
+    enablePopoverAttachment();
+  }
+}
+
+// Attach popover for an element found by id from search input
+document.getElementById('attach-popover').onclick = function () {
+  const shapeElt = bpmnVisualization.htmlElementRegistry.getBpmnHtmlElement(searchElt.value);
+  shapeElt.classList.add(popoverForeignObjectClass);
+  const foreignObjectWrapperElt = createForeignObjectContainer(shapeElt);
+  const popoverDivElt = createPopoverDiv();
+  foreignObjectWrapperElt.prepend(popoverDivElt);
+  shapeElt.append(foreignObjectWrapperElt);
+  setPopoverStyle(popoverDivElt);
+  foreignObjectWrapperElt.setAttribute('style', 'overflow: visible;');
+  disablePopoverAttachment();
+}
+
+// Detach popover for an element found by id from search input if it exists
+document.getElementById('detach-popover').onclick = function () {
+  const shapeElt = bpmnVisualization.htmlElementRegistry.getBpmnHtmlElement(searchElt.value);
+  if (!shapeElt) {
+    return;
+  }
+  if (shapeElt.classList.contains(popoverForeignObjectClass)) {
+    shapeElt.classList.remove(popoverForeignObjectClass);
+    shapeElt.removeChild(shapeElt.lastChild);
+    enablePopoverAttachment();
+  }
+}
+
+function createPopoverDiv() {
+  const popoverDivElt = document.createElement('div');
+  const popoverContent = document.getElementById('popover-content');
+  popoverDivElt.innerHTML = popoverContent.value;
+  return popoverDivElt;
+}
+
+function createForeignObjectContainer(shapeElt) {
+  const rectElt = shapeElt.getElementsByTagName('rect')[0];
+  const foreignObjectElt = document.createElementNS('http://www.w3.org/2000/svg', "foreignObject");
+  foreignObjectElt.setAttribute('width', '100%');
+  foreignObjectElt.setAttribute('height', '100%');
+  foreignObjectElt.setAttribute('x', rectElt.getAttribute('x'));
+  foreignObjectElt.setAttribute('y', rectElt.getAttribute('y'));
+  foreignObjectElt.setAttribute('width', rectElt.getAttribute('width'));
+  foreignObjectElt.setAttribute('height', rectElt.getAttribute('height'));
+  foreignObjectElt.classList.add('bpmn-popover-foreign');
+  return foreignObjectElt;
+}
+
+function setPopoverStyle(popoverContainer) {
+  popoverContainer.classList.add('hidden');
+  popoverContainer.classList.add('bpmn-popover');
+  popoverContainer.setAttribute('style', 'position: absolute;');
+  const boundingRect = popoverContainer.getBoundingClientRect();
+  const popoverHeight = boundingRect.height;
+  // speech bubble indicator height is 21
+  const style = `position: absolute; top: -${popoverHeight + 21}px`;
+  popoverContainer.setAttribute('style', style);
+  popoverContainer.classList.remove('hidden');
+}
+
+function disablePopoverAttachment() {
+  setButtonState('attach-popover', true);
+  setButtonState('detach-popover', false);
+}
+
+function enablePopoverAttachment() {
+  setButtonState('attach-popover', false);
+  setButtonState('detach-popover', true);
+}
