353b1f050faa17fa7f40c51218e7045dd3b0e800
Céline Souchet <celine.souchet@bonitasoft.com>
2020-12-14 16:29:51 +0100

    Update script

--- scripts/update-lib-version.bash
+++ scripts/update-lib-version.bash
@@ -18,10 +18,10 @@ pushd "$EXAMPLES_DIRECTORY" > /dev/null
 echo "Search for files in $(pwd)"
 # lines to update contains substring like "bpmn-visualization@x.y.z"
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	sed -i '' -E "s/\/bpmn-visualization@[0-9].[0-9].[0-9]/\/bpmn-visualization@$NEW_VERSION/" **/*.{html,md,js}
+	sed -i '' -E "s/\/bpmn-visualization@[0-9].[0-9].[0-9]/\/bpmn-visualization@$NEW_VERSION/" **/**/*.{html,md,js}
 	sed -i '' -E "s/\"bpmn-visualization\": \"[0-9].[0-9].[0-9]\"/\"bpmn-visualization\": \"$NEW_VERSION\"/" **/**/package.json
 else
-	sed -i -E "s/\/bpmn-visualization@[0-9].[0-9].[0-9]/\/bpmn-visualization@$NEW_VERSION/#" **/*.{html,md,js} *.{html,js}
+	sed -i -E "s/\/bpmn-visualization@[0-9].[0-9].[0-9]/\/bpmn-visualization@$NEW_VERSION/#" **/**/*.{html,md,js} *.{html,js}
 	sed -i -E "s/\"bpmn-visualization\": \"[0-9].[0-9].[0-9]\"/\"bpmn-visualization\": \"$NEW_VERSION\"/#" **/**/package.json
 fi
 
