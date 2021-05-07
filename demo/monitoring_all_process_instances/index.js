89cf7d62196f47a64119fca1e67ce1d9a2be8da2
Celine Souchet <celine.souchet@bonitasoft.com>
2021-05-07 18:09:28 +0200

    Add handler on radio buttons

--- demo/monitoring_all_process_instances/index.js
+++ demo/monitoring_all_process_instances/index.js
@@ -22,12 +22,44 @@
     }
 };*/
 
-// Initialize BpmnVisualization
-const bpmnVisualization = new bpmnvisu.BpmnVisualization({container: 'bpmn-container', navigation: {enabled: true}});
-const bpmnElementsRegistry = bpmnVisualization.bpmnElementsRegistry;
+
+// Initialize BpmnVisualization for Time Data
+const timeBpmnVisualization = new bpmnvisu.BpmnVisualization({container: 'time-bpmn-container', navigation: {enabled: true}});
+const timeBpmnElementsRegistry = timeBpmnVisualization.bpmnElementsRegistry;
 
 // Load BPMN diagram
-bpmnVisualization.load(getHardwareRetailerDiagram(), {fit: {type: 'Center', margin: 30}});
+timeBpmnVisualization.load(getHardwareRetailerDiagram(), {fit: {type: 'Center', margin: 30}});
+
+
+
+// Initialize BpmnVisualization for Frequency Data
+const frequencyBpmnVisualization = new bpmnvisu.BpmnVisualization({container: 'frequency-bpmn-container', navigation: {enabled: true}});
+const frequencyBpmnElementsRegistry = frequencyBpmnVisualization.bpmnElementsRegistry;
+
+
+
+let frequencyBpmnDiagramIsAlreadyLoad = false;
+document.getElementById('switch-panel').onclick = () => {
+    let switchId = document.querySelector("input[type='radio'][name='switch-data-type']:checked").id;
+    switchDiagram(switchId==='btn-time'? 'time' : 'frequency');
+}
+
+function switchDiagram(switchValue) {
+    // Display corresponding BPMN container & Hide others
+    const bpmnContainers = document.getElementsByClassName("bpmn-container");
+    for (let i = 0; i < bpmnContainers.length; i++) {
+        bpmnContainers.item(i).classList.add('d-hide');
+    }
+    document.getElementById(`${switchValue}-bpmn-container`).classList.remove('d-hide');
+
+    // Load BPMN diagram for Frequency Data, if it's not already done
+    if(switchValue==='frequency' && !frequencyBpmnDiagramIsAlreadyLoad) {
+        frequencyBpmnVisualization.load(getHardwareRetailerDiagram(), { fit: {type: 'Center', margin: 30 } });
+        frequencyBpmnDiagramIsAlreadyLoad = true;
+    }
+}
+
+
 
 /*document.getElementById('btn-set-overlay').onclick = () => {
     removeAllOverlays();
