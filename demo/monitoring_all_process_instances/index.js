27cfdeeb8452537178242abfce2d60747f93ac2f
Celine Souchet <celine.souchet@bonitasoft.com>
2021-05-07 17:42:44 +0200

    Add 'Switch' button to select the data that we want to display

--- demo/monitoring_all_process_instances/index.js
+++ demo/monitoring_all_process_instances/index.js
@@ -1,20 +1,15 @@
-function removeAllOverlays() {
+/*function removeAllOverlays() {
     // Remove all overlays
     // Shape
     bpmnElementsRegistry.removeAllOverlays('Activity_1potg3p');
 
     // Edge
     bpmnElementsRegistry.removeAllOverlays('Flow_1wkfbb0');
-}
+}*/
 
 
-// Initialize the panel of Overlay settings
-var fontColorElt = document.getElementById('font-color');
-var fontSizeElt = document.getElementById('font-size');
-var fillColorElt = document.getElementById('fill-color');
-var strokeColorElt = document.getElementById('stroke-color');
 
-var style = {
+/*var style = {
     font: {
         color: fontColorElt.value,
         size: fontSizeElt.value,
@@ -25,12 +20,7 @@ var style = {
     stroke: {
         color: strokeColorElt.value,
     }
-};
-
-fontColorElt.onchange = (event) => style.font.color = event.target.value;
-fontSizeElt.onchange = (event) => style.font.size = event.target.value;
-fillColorElt.onchange = (event) => style.fill.color = event.target.value;
-strokeColorElt.onchange = (event) => style.stroke.color = event.target.value;
+};*/
 
 // Initialize BpmnVisualization
 const bpmnVisualization = new bpmnvisu.BpmnVisualization({container: 'bpmn-container', navigation: {enabled: true}});
@@ -39,7 +29,7 @@ const bpmnElementsRegistry = bpmnVisualization.bpmnElementsRegistry;
 // Load BPMN diagram
 bpmnVisualization.load(getHardwareRetailerDiagram(), {fit: {type: 'Center'}});
 
-document.getElementById('btn-set-overlay').onclick = () => {
+/*document.getElementById('btn-set-overlay').onclick = () => {
     removeAllOverlays();
 
     // Add overlays
@@ -48,9 +38,9 @@ document.getElementById('btn-set-overlay').onclick = () => {
 
     // Edge
     bpmnElementsRegistry.addOverlays('Flow_1wkfbb0', {position: 'middle', label: '763', style});
-};
+};*/
 
-document.getElementById('btn-reset').onclick = () => {
+/*document.getElementById('btn-reset').onclick = () => {
     removeAllOverlays();
     bpmnVisualization.fit({type: 'Center'});
-};
+};*/
