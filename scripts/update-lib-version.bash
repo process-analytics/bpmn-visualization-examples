249dfd15bf06cdadfaa74219c4dec0712d4c5ce7
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-05-17 18:02:30 +0200

    [INFRA] Fix script to update the lib version in the monitoring demo
    The script was only able to update the examples, it has been refactored to also
    manage assets in the demo folder.

--- scripts/update-lib-version.bash
+++ scripts/update-lib-version.bash
@@ -1,6 +1,37 @@
 #! /bin/bash
 set -e
 
+function replaceVersion() {
+  directory=$1
+  pushd "$directory" > /dev/null
+  echo "Search for files in $(pwd)"
+
+  rexep_npm="s/\"bpmn-visualization\": \".*\"/\"bpmn-visualization\": \"$NEW_VERSION\"/"
+  # lines to update contains substring like "bpmn-visualization@x.y.z"
+  rexep_others="s/\/bpmn-visualization@.*\/dist/\/bpmn-visualization@$NEW_VERSION\/dist/"
+
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    if [[ $directory == *demo ]]; then
+      sed -i '' -E "${rexep_others}" **/*.{html,md,js}
+    else
+      sed -i '' -E "${rexep_others}" **/**/*.{html,md,js}
+      sed -i '' -E "${rexep_npm}" **/**/package.json
+    fi
+  else
+    if [[ $directory == *demo ]]; then
+      sed -i -E "${rexep_others}#" **/*.{html,md,js}
+    else
+      sed -i -E "${rexep_others}#" **/**/*.{html,md,js}
+      sed -i -E "${rexep_npm}#" **/**/package.json
+    fi
+  fi
+
+  echo "Files updated"
+  popd > /dev/null
+  pwd
+}
+
+
 ############################################################
 # update all html files in the examples folder
 ############################################################
@@ -9,26 +40,9 @@ if [ $# -ne 1 ]; then
 	exit 1
 fi
 NEW_VERSION=$1
-echo "Updating examples to make them use demo@$NEW_VERSION"
+echo "Updating examples to make them use bpmn-visualization@$NEW_VERSION"
 
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-EXAMPLES_DIRECTORY="$SCRIPT_DIRECTORY/../examples"
-pushd "$EXAMPLES_DIRECTORY" > /dev/null
 
-echo "Search for files in $(pwd)"
-
-rexep_npm="s/\"bpmn-visualization\": \".*\"/\"bpmn-visualization\": \"$NEW_VERSION\"/"
-# lines to update contains substring like "bpmn-visualization@x.y.z"
-rexep_others="s/\/bpmn-visualization@.*\/dist/\/bpmn-visualization@$NEW_VERSION\/dist/"
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-	sed -i '' -E "${rexep_others}" **/**/*.{html,md,js}
-	sed -i '' -E "${rexep_npm}" **/**/package.json
-else
-	sed -i -E "${rexep_others}#" **/**/*.{html,md,js}
-	sed -i -E "${rexep_npm}#" **/**/package.json
-fi
-
-echo "Files updated"
-popd > /dev/null
-pwd
+replaceVersion "$SCRIPT_DIRECTORY/../examples"
+replaceVersion "$SCRIPT_DIRECTORY/../demo"
