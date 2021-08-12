6c11bd3998ac2298125afb1d89c7fa26926574a9
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-08-12 17:54:02 +0200

    WIP scrollbars

--- examples/diagram-navigation/diagram-navigation/index.js
+++ examples/diagram-navigation/diagram-navigation/index.js
@@ -1,8 +1,19 @@
-console.info('je suis la')
-
 const bpmnVisualization = new bpmnvisu.BpmnVisualization({ container: 'bpmn-container', navigation: { enabled: true } });
 bpmnVisualization.load(getNavigationBpmnDiagram());
 
 document.getElementById('btn-reset').onclick = function() {
   bpmnVisualization.fit({type: bpmnvisu.FitType.None});
 };
+
+
+let displayScrollBars = false;
+document.getElementById('btn-toggle-scrollbars').onclick = function() {
+  displayScrollBars = !displayScrollBars;
+
+  document.getElementById('bpmn-container').style.overflow = displayScrollBars ? 'auto' : 'hidden';
+  // If default (true) value is used, the regular panning is not used, so
+  // if previous panning occured this can prevent to see the diagram
+  // bpmnVisualization.graph.useScrollbarsForPanning = false;
+};
+
+
