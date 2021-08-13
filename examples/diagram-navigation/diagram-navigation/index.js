bc50876eea603ac8df33e4cc23aa61140054e203
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-08-13 11:11:35 +0200

    wip: button to display scrollbars or not

--- examples/diagram-navigation/diagram-navigation/index.js
+++ examples/diagram-navigation/diagram-navigation/index.js
@@ -5,6 +5,15 @@ document.getElementById('btn-reset').onclick = function() {
   bpmnVisualization.fit({type: bpmnvisu.FitType.None});
 };
 
+function resetZoomLevel() {
+  bpmnVisualization.fit({type: bpmnvisu.FitType.None});
+}
+
+// function loadInNewInstance() {
+//   const instance = new bpmnvisu.BpmnVisualization({ container: 'bpmn-container', navigation: { enabled: true } });
+// instance.load(getNavigationBpmnDiagram());
+// return instance;
+// }
 
 let displayScrollBars = false;
 document.getElementById('btn-toggle-scrollbars').onclick = function() {
@@ -14,6 +23,14 @@ document.getElementById('btn-toggle-scrollbars').onclick = function() {
   // If default (true) value is used, the regular panning is not used, so
   // if previous panning occured this can prevent to see the diagram
   // bpmnVisualization.graph.useScrollbarsForPanning = false;
+  
+  // resetZoomLevel();
+
+  // ensure that all diagram positions are recomputed
+  bpmnVisualization.load(getNavigationBpmnDiagram());
+
+
+  //bpmnVisualization = loadInNewInstance();
 };
 
 
