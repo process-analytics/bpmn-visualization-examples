22e48a47947f18935f438e136d5e355fb44df0de
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-08-13 11:40:22 +0200

    clean code

--- examples/diagram-navigation/diagram-navigation/index.js
+++ examples/diagram-navigation/diagram-navigation/index.js
@@ -1,41 +1,22 @@
-const bpmnVisualization = new bpmnvisu.BpmnVisualization({ container: 'bpmn-container', navigation: { enabled: true } });
+const bpmnContainer = document.getElementById('bpmn-container');
+const bpmnVisualization = new bpmnvisu.BpmnVisualization({ container: bpmnContainer, navigation: { enabled: true } });
 bpmnVisualization.load(getNavigationBpmnDiagram());
 
-document.getElementById('btn-reset').onclick = function() {
-  bpmnVisualization.fit({type: bpmnvisu.FitType.None});
+let displayScrollBars = false;
+document.getElementById('btn-reset').onclick = function () {
+  bpmnVisualization.fit({ type: bpmnvisu.FitType.None });
 
-  if(displayScrollBars){
-    // reset scrollbars position
-    document.getElementById('bpmn-container').scrollTo(0, 0);
-  }
+  // reset scrollbars position
+  displayScrollBars && bpmnContainer.scrollTo(0, 0);
 };
 
-function resetZoomLevel() {
-  bpmnVisualization.fit({type: bpmnvisu.FitType.None});
-}
-
-// function loadInNewInstance() {
-//   const instance = new bpmnvisu.BpmnVisualization({ container: 'bpmn-container', navigation: { enabled: true } });
-// instance.load(getNavigationBpmnDiagram());
-// return instance;
-// }
-
-let displayScrollBars = false;
-document.getElementById('btn-toggle-scrollbars').onclick = function() {
+document.getElementById('btn-toggle-scrollbars').onclick = function () {
   displayScrollBars = !displayScrollBars;
 
-  document.getElementById('bpmn-container').style.overflow = displayScrollBars ? 'auto' : 'hidden';
-  // If default (true) value is used, the regular panning is not used, so
-  // if previous panning occured this can prevent to see the diagram
-  // bpmnVisualization.graph.useScrollbarsForPanning = false;
-  
-  // resetZoomLevel();
+  bpmnContainer.style.overflow = displayScrollBars ? 'auto' : 'hidden';
 
-  // ensure that all diagram positions are recomputed
+  // ensure that all diagram positions are recomputed (mainly for no scrollbars)
   bpmnVisualization.load(getNavigationBpmnDiagram());
-
-
-  //bpmnVisualization = loadInNewInstance();
 };
 
 
