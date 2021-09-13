3846015cb757ea6bad5ceea90244b563845d3e8e
Souchet Céline <4921914+csouchet@users.noreply.github.com>
2021-09-13 17:40:06 +0200

    [REFACTOR] Demo time/frequency monitoring: Introduce state for buttons (#232)
    * Fix: Change the event, to avoid unnecessary handlers
    * Add state for current use case & data type

--- demo/monitoring-all-process-instances/js/index.js
+++ demo/monitoring-all-process-instances/js/index.js
@@ -1,28 +1,29 @@
-document.getElementById('btn-time').checked = true;
-document.getElementById('btn-both').checked = true;
-
 // Initialize UseCases
 const timeUseCase = new UseCase('time', new TimeExecutionData());
 const frequencyUseCase = new UseCase('frequency', new FrequencyExecutionData());
 
+// Initialize state of radio buttons
+const state = {
+    useCase : timeUseCase,
+    dataType : 'both'
+}
+document.getElementById('btn-time').checked = true;
+document.getElementById(`btn-${state.dataType}`).checked = true;
+
 document.addEventListener('DOMContentLoaded', function () {
     // Waiting for the displayed page before to load diagram & display data
-    const dataType = document.querySelector("input[type='radio'][name='data-type']:checked").value;
-    timeUseCase.display(dataType);
+    state.useCase.display(state.dataType);
 })
 
-document.getElementById('choose-use-case-panel').onclick = () => {
+document.getElementById('choose-use-case-panel').onchange = () => {
     const useCaseType = document.querySelector("input[type='radio'][name='use-case-type']:checked").value;
-    const dataType = document.querySelector("input[type='radio'][name='data-type']:checked").value;
+    state.useCase = useCaseType === 'frequency' ? frequencyUseCase : timeUseCase;
 
-    const useCase = useCaseType === 'frequency' ? frequencyUseCase : timeUseCase;
-    useCase.display(dataType);
+    state.useCase.display(state.dataType);
 }
 
-document.getElementById('choose-data-panel').onclick = () => {
-    const useCaseType = document.querySelector("input[type='radio'][name='use-case-type']:checked").value;
-    const dataType = document.querySelector("input[type='radio'][name='data-type']:checked").value;
+document.getElementById('choose-data-panel').onchange = () => {
+    state.dataType = document.querySelector("input[type='radio'][name='data-type']:checked").value;
 
-    const useCase = useCaseType === 'frequency' ? frequencyUseCase : timeUseCase;
-    useCase.displayData(dataType);
+    state.useCase.displayData(state.dataType);
 }
