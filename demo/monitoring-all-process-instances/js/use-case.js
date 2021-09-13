3a0860bac5ab381fd141b9de4602b78a24c58eb7
Celine Souchet <celine.souchet@bonitasoft.com>
2021-09-13 18:23:44 +0200

    Don't remove legends for all data call

--- demo/monitoring-all-process-instances/js/use-case.js
+++ demo/monitoring-all-process-instances/js/use-case.js
@@ -14,9 +14,9 @@ class UseCase {
     display(dataType) {
         this._displayPanel();
 
-        if(!this.#alreadyLoad) {
+        if (!this.#alreadyLoad) {
             this.#bpmnVisualization.load(getHardwareRetailerDiagram(), {fit: {type: 'Center', margin: 30}});
-            this.#alreadyLoad  = true;
+            this.#alreadyLoad = true;
         }
 
         this.displayData(dataType);
@@ -24,8 +24,45 @@ class UseCase {
 
     displayData(dataType) {
         console.info('Setting %s data', dataType);
-        let dataActionOnDisplay = this._getDataActionOnDisplay(dataType);
-        this.#executionData.data.forEach((value, key) =>  dataActionOnDisplay(key, value));
+        switch (dataType) {
+            case 'overlays':
+                this.#executionData.hidePathLegend();
+                this.#executionData.displayOverlaysLegends();
+                this.#executionData.data.forEach((value, key) => {
+                    if (value.pathClass) {
+                        this.#bpmnVisualization.bpmnElementsRegistry.removeCssClasses(key, value.pathClass);
+                    }
+
+                    if (value.overlay) {
+                        this.#bpmnVisualization.bpmnElementsRegistry.addOverlays(key, value.overlay);
+                    }
+                });
+                break;
+            case 'paths':
+                this.#executionData.hideOverlaysLegends();
+                this.#executionData.displayPathLegend();
+                this.#executionData.data.forEach((value, key) => {
+                    this.#bpmnVisualization.bpmnElementsRegistry.removeAllOverlays(key);
+
+                    if (value.pathClass) {
+                        this.#bpmnVisualization.bpmnElementsRegistry.addCssClasses(key, value.pathClass);
+                    }
+                });
+                break;
+            case 'both':
+            default:
+                this.#executionData.displayPathLegend();
+                this.#executionData.displayOverlaysLegends();
+                this.#executionData.data.forEach((value, key) => {
+                    if (value.pathClass) {
+                        this.#bpmnVisualization.bpmnElementsRegistry.addCssClasses(key, value.pathClass);
+                    }
+
+                    if (value.overlay) {
+                        this.#bpmnVisualization.bpmnElementsRegistry.addOverlays(key, value.overlay);
+                    }
+                });
+        }
         console.info('%s data set', dataType);
     }
 
@@ -47,45 +84,5 @@ class UseCase {
         // Display corresponding HTML element
         document.getElementById(`${this.#type}-${subId}`).classList.remove('d-hide');
         console.info('%s displayed', `${this.#type}-${subId}`);
-    }
-
-    _getDataActionOnDisplay(dataType) {
-        switch (dataType) {
-            case 'overlays':
-                return (key, value) => {
-                    if (value.pathClass) {
-                        this.#bpmnVisualization.bpmnElementsRegistry.removeCssClasses(key, value.pathClass);
-                    }
-                    this.#executionData.hidePathLegend();
-
-                    if (value.overlay) {
-                        this.#bpmnVisualization.bpmnElementsRegistry.addOverlays(key, value.overlay);
-                    }
-                    this.#executionData.displayOverlaysLegends();
-                };
-            case 'paths':
-                return (key, value) => {
-                    this.#bpmnVisualization.bpmnElementsRegistry.removeAllOverlays(key);
-                    this.#executionData.hideOverlaysLegends();
-
-                    if (value.pathClass) {
-                        this.#bpmnVisualization.bpmnElementsRegistry.addCssClasses(key, value.pathClass);
-                    }
-                    this.#executionData.displayPathLegend();
-                };
-            case 'both':
-            default:
-                return (key, value) => {
-                    if (value.pathClass) {
-                        this.#bpmnVisualization.bpmnElementsRegistry.addCssClasses(key, value.pathClass);
-                    }
-                    this.#executionData.displayPathLegend();
-
-                    if (value.overlay) {
-                        this.#bpmnVisualization.bpmnElementsRegistry.addOverlays(key, value.overlay);
-                    }
-                    this.#executionData.displayOverlaysLegends();
-                };
-        }
     }
 }
