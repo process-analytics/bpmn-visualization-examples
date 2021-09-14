844a8a0d0ad25a23b73d9e54b023750c6c8e549b
Celine Souchet <celine.souchet@bonitasoft.com>
2021-09-14 11:40:45 +0200

    Extract abstract UseCase class from monitoring example

---
+++ demo/monitoring-all-process-instances/js/monitoring-use-case.js
@@ -0,0 +1,63 @@
+class MonitoringUseCase extends UseCase{
+    #executionData;
+
+    constructor(type, getDiagram, executionData) {
+        super(type, getDiagram, true);
+        this.#executionData = executionData;
+    }
+
+    display(dataType) {
+        super.display(dataType);
+        this.displayData(dataType);
+    }
+
+    displayData(dataType) {
+        console.info('Setting %s data', dataType);
+        switch (dataType) {
+            case 'overlays':
+                this.#executionData.hidePathLegend();
+                this.#executionData.displayOverlaysLegends();
+                this.#executionData.data.forEach((value, key) => {
+                    if (value.pathClass) {
+                        this._bpmnVisualization.bpmnElementsRegistry.removeCssClasses(key, value.pathClass);
+                    }
+
+                    if (value.overlay) {
+                        this._bpmnVisualization.bpmnElementsRegistry.addOverlays(key, value.overlay);
+                    }
+                });
+                break;
+            case 'paths':
+                this.#executionData.hideOverlaysLegends();
+                this.#executionData.displayPathLegend();
+                this.#executionData.data.forEach((value, key) => {
+                    this._bpmnVisualization.bpmnElementsRegistry.removeAllOverlays(key);
+
+                    if (value.pathClass) {
+                        this._bpmnVisualization.bpmnElementsRegistry.addCssClasses(key, value.pathClass);
+                    }
+                });
+                break;
+            case 'both':
+            default:
+                this.#executionData.displayPathLegend();
+                this.#executionData.displayOverlaysLegends();
+                this.#executionData.data.forEach((value, key) => {
+                    if (value.pathClass) {
+                        this._bpmnVisualization.bpmnElementsRegistry.addCssClasses(key, value.pathClass);
+                    }
+
+                    if (value.overlay) {
+                        this._bpmnVisualization.bpmnElementsRegistry.addOverlays(key, value.overlay);
+                    }
+                });
+        }
+        console.info('%s data set', dataType);
+    }
+
+
+    _displayPanel() {
+        super._displayPanel();
+        this.#executionData.updateLegends();
+    }
+}
