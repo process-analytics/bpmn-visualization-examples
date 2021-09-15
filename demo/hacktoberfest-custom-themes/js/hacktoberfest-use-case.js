c5ae9dcf3fd0f828376c8f2b6dfc92af3e79c6b9
Celine Souchet <celine.souchet@bonitasoft.com>
2021-09-15 11:35:16 +0200

    Move unnecessary code in common Use Case

--- demo/hacktoberfest-custom-themes/js/hacktoberfest-use-case.js
+++ demo/hacktoberfest-custom-themes/js/hacktoberfest-use-case.js
@@ -1,5 +1,13 @@
 class HacktoberfestUseCase extends UseCase {
 
+    // Default BPMN Visualization theme
+    #originalDefaultFontColor;
+    #originalDefaultFontFamily;
+    #originalDefaultStrokeColor;
+    #originalDefaultFillColor;
+    #originalPoolLabelFillColor;
+    #originalConfigureCommonDefaultStyle;
+
     constructor(type, inputProjectName) {
         super(type, () => getHacktoberfestBpmnDiagram(inputProjectName), false);
     }
@@ -22,5 +30,29 @@ class HacktoberfestUseCase extends UseCase {
      */
     _preLoadDiagram() {
         bpmnvisu.IconPainterProvider.set(new bpmnvisu.IconPainter());
+    }
+
+    /**
+     * Generic implementation
+     */
+    _saveDefaultTheme() {
+        this.#originalDefaultFontColor = bpmnvisu.StyleDefault.DEFAULT_FONT_COLOR;
+        this.#originalDefaultFontFamily = bpmnvisu.StyleDefault.DEFAULT_FONT_FAMILY;
+        this.#originalDefaultStrokeColor = bpmnvisu.StyleDefault.DEFAULT_STROKE_COLOR;
+        this.#originalDefaultFillColor = bpmnvisu.StyleDefault.DEFAULT_FILL_COLOR;
+        this.#originalPoolLabelFillColor = bpmnvisu.StyleDefault.POOL_LABEL_FILL_COLOR;
+        this.#originalConfigureCommonDefaultStyle = bpmnvisu.StyleConfigurator.configureCommonDefaultStyle;
+    }
+
+    /**
+     * Generic implementation
+     */
+    _restoreDefaultTheme() {
+        bpmnvisu.StyleDefault.DEFAULT_FONT_COLOR = this.#originalDefaultFontColor;
+        bpmnvisu.StyleDefault.DEFAULT_FONT_FAMILY = this.#originalDefaultFontFamily;
+        bpmnvisu.StyleDefault.DEFAULT_STROKE_COLOR = this.#originalDefaultStrokeColor;
+        bpmnvisu.StyleDefault.DEFAULT_FILL_COLOR = this.#originalDefaultFillColor;
+        bpmnvisu.StyleDefault.POOL_LABEL_FILL_COLOR = this.#originalPoolLabelFillColor;
+        bpmnvisu.StyleConfigurator.configureCommonDefaultStyle = this.#originalConfigureCommonDefaultStyle;
     }
 }
--- demo/static/js/use-case.js
+++ demo/static/js/use-case.js
@@ -1,27 +1,21 @@
 class UseCase {
     #type;
     #getDiagram;
+    #navigationEnabled;
     _bpmnVisualization;
     #alreadyLoad = false;
-
-    // Default BPMN Visualization theme
-    #originalDefaultFontColor;
-    #originalDefaultFontFamily;
-    #originalDefaultStrokeColor;
-    #originalDefaultFillColor;
-    #originalPoolLabelFillColor;
-    #originalConfigureCommonDefaultStyle;
 
     constructor(type, getDiagram, navigationEnabled) {
         this.#type = type;
         this.#getDiagram = getDiagram;
-        this._bpmnVisualization = this._initBpmnVisualization({container: `${type}-bpmn-container`, navigation: {enabled: navigationEnabled}});
+        this.#navigationEnabled = navigationEnabled;
     }
 
     display(dataType) {
         this._displayPanel();
 
         if (!this.#alreadyLoad) {
+            this._bpmnVisualization = this._initBpmnVisualization({container: `${this.#type}-bpmn-container`, navigation: {enabled: this.#navigationEnabled}});
             this._preLoadDiagram();
             this._bpmnVisualization.load(this.#getDiagram(), {fit: {type: 'Center', margin: 30}});
             this.#alreadyLoad = true;
@@ -64,27 +58,4 @@ class UseCase {
         console.info('%s displayed', `${this.#type}-${subId}`);
     }
 
-    /**
-     * Generic implementation
-     */
-    _saveDefaultTheme() {
-        this.#originalDefaultFontColor = bpmnvisu.StyleDefault.DEFAULT_FONT_COLOR;
-        this.#originalDefaultFontFamily = bpmnvisu.StyleDefault.DEFAULT_FONT_FAMILY;
-        this.#originalDefaultStrokeColor = bpmnvisu.StyleDefault.DEFAULT_STROKE_COLOR;
-        this.#originalDefaultFillColor = bpmnvisu.StyleDefault.DEFAULT_FILL_COLOR;
-        this.#originalPoolLabelFillColor = bpmnvisu.StyleDefault.POOL_LABEL_FILL_COLOR;
-        this.#originalConfigureCommonDefaultStyle = bpmnvisu.StyleConfigurator.configureCommonDefaultStyle;
-    }
-
-    /**
-     * Generic implementation
-     */
-    _restoreDefaultTheme() {
-        bpmnvisu.StyleDefault.DEFAULT_FONT_COLOR = this.#originalDefaultFontColor;
-        bpmnvisu.StyleDefault.DEFAULT_FONT_FAMILY = this.#originalDefaultFontFamily;
-        bpmnvisu.StyleDefault.DEFAULT_STROKE_COLOR = this.#originalDefaultStrokeColor;
-        bpmnvisu.StyleDefault.DEFAULT_FILL_COLOR = this.#originalDefaultFillColor;
-        bpmnvisu.StyleDefault.POOL_LABEL_FILL_COLOR = this.#originalPoolLabelFillColor;
-        bpmnvisu.StyleConfigurator.configureCommonDefaultStyle = this.#originalConfigureCommonDefaultStyle;
-    }
 }
