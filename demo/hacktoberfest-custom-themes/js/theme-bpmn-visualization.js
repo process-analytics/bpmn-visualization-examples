487576e99cec8bb1f702db383f3c15f25b3b3948
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-09-16 12:00:43 +0200

    REBASE: hacktoberfest-theme demo - remove mxConstants

--- demo/hacktoberfest-custom-themes/js/theme-bpmn-visualization.js
+++ demo/hacktoberfest-custom-themes/js/theme-bpmn-visualization.js
@@ -12,48 +12,48 @@ class ThemeBpmnVisualization extends bpmnvisu.BpmnVisualization {
 
         // START EVENT
         let style = styleSheet.styles[bpmnvisu.ShapeBpmnElementKind.EVENT_START];
-        style[bpmnvisu.mxConstants.STYLE_STROKECOLOR] = this._theme.startEvent.stroke;
+        style[StyleIdentifiers.STYLE_STROKECOLOR] = this._theme.startEvent.stroke;
         if (this._theme.startEvent.fill) {
-            style[bpmnvisu.mxConstants.STYLE_FILLCOLOR] = this._theme.startEvent.fill;
+            style[StyleIdentifiers.STYLE_FILLCOLOR] = this._theme.startEvent.fill;
         }
 
         // END EVENT
         style = styleSheet.styles[bpmnvisu.ShapeBpmnElementKind.EVENT_END];
-        style[bpmnvisu.mxConstants.STYLE_STROKECOLOR] = this._theme.endEvent.stroke;
-        style[bpmnvisu.mxConstants.STYLE_FILLCOLOR] = this._theme.endEvent.fill;
+        style[StyleIdentifiers.STYLE_STROKECOLOR] = this._theme.endEvent.stroke;
+        style[StyleIdentifiers.STYLE_FILLCOLOR] = this._theme.endEvent.fill;
         if (this._theme.endEvent.gradient) {
-            style[bpmnvisu.mxConstants.STYLE_GRADIENT_DIRECTION] = bpmnvisu.mxConstants.DIRECTION_WEST;
-            style[bpmnvisu.mxConstants.STYLE_GRADIENTCOLOR] = this._theme.endEvent.gradient;
+            style[StyleIdentifiers.STYLE_GRADIENT_DIRECTION] = Directions.DIRECTION_WEST;
+            style[StyleIdentifiers.STYLE_GRADIENTCOLOR] = this._theme.endEvent.gradient;
         }
 
         // USER TASK
         style = styleSheet.styles[bpmnvisu.ShapeBpmnElementKind.TASK_USER];
-        style[bpmnvisu.mxConstants.STYLE_FILLCOLOR] = this._theme.userTask.fill;
+        style[StyleIdentifiers.STYLE_FILLCOLOR] = this._theme.userTask.fill;
         if (this._theme.userTask.font) {
-            style[bpmnvisu.mxConstants.STYLE_FONTCOLOR] = this._theme.userTask.font;
+            style[StyleIdentifiers.STYLE_FONTCOLOR] = this._theme.userTask.font;
         }
 
         // EXCLUSIVE GATEWAY
         if (this._theme.exclusiveGateway.fill) {
             style = styleSheet.styles[bpmnvisu.ShapeBpmnElementKind.GATEWAY_EXCLUSIVE];
-            style[bpmnvisu.mxConstants.STYLE_FILLCOLOR] = this._theme.exclusiveGateway.fill;
+            style[StyleIdentifiers.STYLE_FILLCOLOR] = this._theme.exclusiveGateway.fill;
         }
 
         // CALL ACTIVITY
         style = styleSheet.styles[bpmnvisu.ShapeBpmnElementKind.CALL_ACTIVITY];
-        style[bpmnvisu.mxConstants.STYLE_FILLCOLOR] = this._theme.callActivity.fill;
-        style[bpmnvisu.mxConstants.STYLE_FONTCOLOR] = this._theme.callActivity.font;
+        style[StyleIdentifiers.STYLE_FILLCOLOR] = this._theme.callActivity.fill;
+        style[StyleIdentifiers.STYLE_FONTCOLOR] = this._theme.callActivity.font;
         if (this._theme.callActivity.stroke) {
-            style[bpmnvisu.mxConstants.STYLE_STROKECOLOR] = this._theme.callActivity.stroke;
+            style[StyleIdentifiers.STYLE_STROKECOLOR] = this._theme.callActivity.stroke;
         }
 
         // POOL
         style = styleSheet.styles[bpmnvisu.ShapeBpmnElementKind.POOL];
-        style[bpmnvisu.mxConstants.STYLE_FILLCOLOR] = this._theme.pool.labelFill;
-        style[bpmnvisu.mxConstants.STYLE_SWIMLANE_FILLCOLOR] = this._theme.pool.swimlaneFill;
-        style[bpmnvisu.mxConstants.STYLE_FONTSIZE] = 16;
+        style[StyleIdentifiers.STYLE_FILLCOLOR] = this._theme.pool.labelFill;
+        style[StyleIdentifiers.STYLE_SWIMLANE_FILLCOLOR] = this._theme.pool.swimlaneFill;
+        style[StyleIdentifiers.STYLE_FONTSIZE] = 16;
         if (this._theme.pool.font) {
-            style[bpmnvisu.mxConstants.STYLE_FONTCOLOR] = this._theme.pool.font;
+            style[StyleIdentifiers.STYLE_FONTCOLOR] = this._theme.pool.font;
         }
     }
 
