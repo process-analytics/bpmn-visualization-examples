60f8451c67d552e6e81968b654284f5874b60e05
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-11-22 15:46:13 +0100

    [FEAT] 'Predictions' demo: highlight predicted paths
    Highlight predicted path on hover on predicted late or on-time task.
    The style of running elements is toggled when displaying the predicted path to
    avoid displaying too much information.
    
    Other changes
    Display:
      - reduce fit margin to enlarge the diagram
      - diagram: increase 'pizza received' label width for no-wrap on highlight
    CSS
      - simplify definition of the pulse-animation
      - simplify styles by using the generic BPMN CSS classes introduced in v0.21.0
    Refactor use-case:
      - allow passing load options to parent UseCase
      - allow registering style/interactions after diagram load

--- demo/predictions/js/prediction-use-case.js
+++ demo/predictions/js/prediction-use-case.js
@@ -19,49 +19,105 @@ const alreadyExecutedElementsPredictedLate = [
 
 const vendorBakeThePizzaId = '_6-463';
 const vendorWhereIsMyPizzaId = '_6-674';
-const vendorDeliverThePizzaId = '_6-514';
 const customerEvtBasedGwId = '_6-180';
 
 class PredicatedLateUseCase extends UseCase {
     _alreadyExecutedElements;
+    _predictionData = {
+        elementWithPrediction: {
+            bpmnId: vendorBakeThePizzaId,
+            cssClasses: 'state-predicted-late',
+        },
+        predictedPath: {
+            ids: [
+                // customer elements
+                customerEvtBasedGwId,
+                '_6-424', // sequence flow between 'event-based gateway' (running element) and timer event
+                '_6-219', // timer event
+                '_6-426', // sequence flow between 'timer event' and 'Ask for the pizza'
+                '_6-236', // 'Ask for the pizza'
+                // message flow
+                '_6-642',
+                // vendor elements
+                vendorWhereIsMyPizzaId,
+                '_6-748', // sequence flow between 'where is my pizza' and 'Calm customer'
+                '_6-695', // 'Calm customer'
+            ],
+            cssClasses: 'path-predicted-late',
+        },
+    };
 
     constructor(type) {
-        super(type, () => pizzaDiagram(), true);
+        super(type, () => pizzaDiagram(), true, {fit: {type: 'Center', margin: 20}});
         this._alreadyExecutedElements = alreadyExecutedElementsPredictedLate;
     }
 
-    display(dataType) {
-        super.display(dataType);
+    _postLoadDiagram() {
         this._reduceVisibilityOfAlreadyExecutedElements();
-        this._highlightPredictionOnRunningElements();
+        this._highlightRunningElementsWithPrediction();
+        this._toggleHighlightRunningElementsWithoutPrediction();
+        this._registerInteractions();
     }
 
     _reduceVisibilityOfAlreadyExecutedElements() {
         this._bpmnVisualization.bpmnElementsRegistry.addCssClasses(this._alreadyExecutedElements, 'state-already-executed');
     }
 
-    _highlightPredictionOnRunningElements() {
-        this._bpmnVisualization.bpmnElementsRegistry.addCssClasses(vendorBakeThePizzaId, 'state-predicted-late');
-        this._bpmnVisualization.bpmnElementsRegistry.addCssClasses([vendorWhereIsMyPizzaId, customerEvtBasedGwId], 'state-running');
+    _registerInteractions() {
+        // on hover, highlight the predicted path
+        const bpmnElements = this._bpmnVisualization.bpmnElementsRegistry.getElementsByIds(this._predictionData.elementWithPrediction.bpmnId);
+        const elementTogglingPath = bpmnElements[0]; // exist and only one
+
+        const highlightPredictedPath = () => {
+            this._toggleHighlightRunningElementsWithoutPrediction();
+            this._bpmnVisualization.bpmnElementsRegistry.toggleCssClasses(this._predictionData.predictedPath.ids, this._predictionData.predictedPath.cssClasses);
+        }
+
+        elementTogglingPath.htmlElement.addEventListener('mouseenter', highlightPredictedPath);
+        elementTogglingPath.htmlElement.addEventListener('mouseleave', highlightPredictedPath);
     }
 
+    _toggleHighlightRunningElementsWithoutPrediction() {
+        this._bpmnVisualization.bpmnElementsRegistry.toggleCssClasses([vendorWhereIsMyPizzaId, customerEvtBasedGwId], 'state-running');
+    }
+
+    _highlightRunningElementsWithPrediction() {
+        this._bpmnVisualization.bpmnElementsRegistry.addCssClasses(this._predictionData.elementWithPrediction.bpmnId, this._predictionData.elementWithPrediction.cssClasses);
+    }
 }
 
 
 class PredictedOnTimeUseCase extends PredicatedLateUseCase {
+    _predictionData = {
+        elementWithPrediction: {
+            bpmnId: '_6-514', // vendor 'Deliver the pizza'
+            cssClasses: 'state-predicted-on-time',
+        },
+        predictedPath: {
+            ids: [
+                // customer elements
+                customerEvtBasedGwId,
+                '_6-422', // sequence flow between 'event-based gateway' (running element) and msg event
+                '_6-202', // msg event 'Pizza received'
+                '_6-428', // sequence flow between 'msg event' and 'Pay the pizza'
+                '_6-304', // 'Pay the pizza'
+                // message flow
+                '_6-640', // from vendor 'Deliver the pizza' to customer 'msg event'
+                // vendor elements
+                '_6-634', // sequence flow between 'Deliver the pizza' and 'Receive payment'
+                '_6-565', // 'Receive payment'
+            ],
+            cssClasses: 'path-predicted-on-time'
+        }
+    };
 
     constructor(type) {
         super(type);
         this._alreadyExecutedElements = [...alreadyExecutedElementsPredictedLate, ...[
             // vendor
-            vendorBakeThePizzaId, //'Bake the pizza'
+            vendorBakeThePizzaId,
             '_6-632', // sequence flow between 'Bake the pizza' and 'Deliver the pizza'
         ]];
-    }
-
-    _highlightPredictionOnRunningElements() {
-        this._bpmnVisualization.bpmnElementsRegistry.addCssClasses(vendorDeliverThePizzaId, 'state-predicted-on-time');
-        this._bpmnVisualization.bpmnElementsRegistry.addCssClasses([vendorWhereIsMyPizzaId, customerEvtBasedGwId], 'state-running');
     }
 
 }
