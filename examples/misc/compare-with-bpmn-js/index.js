9b8feff0211eef544dad8fa341a8c0e1d001c377
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2022-01-18 15:35:11 +0100

    [REFACTOR] Remove duplications in comparison with other BPMN vendors (#283)
    Introduce shared classes for library and UI management + a 'state' shared object.
    Examples extend them which reduces duplications a lot.
    
    The shared classes allow a clearer separation between the libs management and actions on the UI.
    Errors are better handled: for instance, the 'Last loaded file' info is no longer updated on load failure.
    
    kie-editors-standalone example: manage the 'Fit View' button title and text based on the 'fitView' state for a better feedback for the user.

--- examples/misc/compare-with-bpmn-js/index.js
+++ examples/misc/compare-with-bpmn-js/index.js
@@ -1,186 +1,38 @@
-const bpmnVisualizationContainerId = 'container-bpmn-visualization';
-const bpmnVisualization = new bpmnvisu.BpmnVisualization({ container: bpmnVisualizationContainerId, navigation: { enabled: true } });
+// the rest of the code is in shared.js (shared with the kie-editors-standalone comparison example)
 
-const bpmnJsContainerId = 'container-bpmn-js';
-const bpmnJsViewer = new BpmnJS({ container: `#${bpmnJsContainerId}` });
+class BpmnJSLibraryComparator extends LibraryComparator {
+  #bpmnJsViewer;
 
-let loadedXml;
-let fitView = false;
-let isBpmnVisualizationAlreadyLoaded = false;
-let isBpmnJsAlreadyLoaded = false;
-
-// Configure custom html elements
-document.getElementById('btn-open-file').addEventListener('change', handleFileSelect, false);
-const bpmnLibraryRadioButtons = document.getElementsByName('bpmnLibrary');
-configureBpmnLibraryRadioButtons();
-configureFitViewButton();
-
-
-function loadWithBpmnVisualization(xml) {
-  if (isBpmnVisualizationAlreadyLoaded) {
-    logBpmnVisu('Diagram already loaded, skipping new load');
-    return;
+  constructor(bpmnVisualizationContainerId, otherLibContainerId, state) {
+    super(bpmnVisualizationContainerId, state);
+    this._otherLibIdentifier = 'bpmn-js';
+    this.#bpmnJsViewer = new BpmnJS({ container: `#${otherLibContainerId}` });
   }
-  logBpmnVisu('Start loading diagram');
-  bpmnVisualization.load(xml, { fit: computeBpmnVisualizationFitOptions() });
-  logBpmnVisu('Diagram loaded');
-  isBpmnVisualizationAlreadyLoaded = true;
-}
 
-async function loadWithBpmnJs(xml) {
-  if (isBpmnJsAlreadyLoaded) {
-    logBpmnJs('Diagram already loaded, skipping new load');
-    return;
+  async _loadWithOtherLib(xml) {
+    return this.#bpmnJsViewer.importXML(xml);
   }
-  logBpmnJs('Starting import XML')
-  await bpmnJsViewer.importXML(xml);
-  logBpmnJs('Import XML done')
-  setZoomLevelBpmnJs();
-  isBpmnJsAlreadyLoaded = true;
-}
 
-function setZoomLevelBpmnJs() {
-  const canvas = bpmnJsViewer.get('canvas');
-  if (fitView) {
-    // 'auto' to zoom into mid
-    canvas.zoom('fit-viewport', 'auto');
-  } else {
-    // pass null to ensure the position of the diagram is taken from the BPMN xml
-    canvas.zoom(1, null);
-  }
-  logBpmnJs(`Zoom level set, fitView ${fitView}`);
-}
-
-function setZoomLevelBpmnVisualization() {
-  bpmnVisualization.fit(computeBpmnVisualizationFitOptions());
-  logBpmnVisu(`Zoom level set, fitView ${fitView}`);
-}
-
-function computeBpmnVisualizationFitOptions() {
-  const fitType = fitView ? bpmnvisu.FitType.Center: bpmnvisu.FitType.None;
-  return { type: fitType, margin: 10 };
-}
-
-function handleFileSelect(evt) {
-  evt.stopPropagation();
-  evt.preventDefault();
-
-  const file = evt.target.files[0];
-  logMain(`New file selected for load: ${file.name}`);
-  const reader = new FileReader();
-  reader.onload = () => {
-    isBpmnVisualizationAlreadyLoaded = false;
-    isBpmnJsAlreadyLoaded = false;
-    loadBpmn(reader.result);
-
-    document.getElementById('loading-info').classList.remove('hidden');
-    document.querySelector('#loading-info > span').innerHTML = `<code>${file.name}</code>`;
-  };
-  reader.readAsText(file);
-}
-
-function loadBpmn(xml) {
-  const useBpmnVisualization = isUseBpmnVisualization();
-  displayBpmnContainer(useBpmnVisualization);
-
-  // effective load
-  try {
-    logMain(`Ready to load BPMN with fitView: ${fitView}`);
-    if (useBpmnVisualization) {
-      loadWithBpmnVisualization(xml);
+  _setZoomLevelOtherLib() {
+    const canvas = this.#bpmnJsViewer.get('canvas');
+    if (this._state.fitView) {
+      // 'auto' to zoom into mid
+      canvas.zoom('fit-viewport', 'auto');
     } else {
-      loadWithBpmnJs(xml);
+      // pass null to ensure the position of the diagram is taken from the BPMN xml
+      canvas.zoom(1, null);
     }
-    loadedXml = xml;
-  } catch (err) {
-    console.error('[main] Unable to load BPMN diagram', err);
+    this._logOtherLib(`Zoom level set, fitView ${this._state.fitView}`);
   }
 }
 
-function reloadAfterBpmnLibrarySwitch() {
-  logMain('BPMN Library switch detected');
-  if (!loadedXml) {
-    return;
-  }
-  logMain('Reloading the BPMN diagram');
-  loadBpmn(loadedXml);
+
+const state = {
+  fitView: false,
+  isUseBpmnVisualization: true,
 }
 
+const libraryComparator = new BpmnJSLibraryComparator('container-bpmn-visualization', 'container-bpmn-js', state);
 
-function configureFitViewButton() {
-  const fitViewElt = document.getElementById('fitView');
-
-  fitView = fitViewElt.checked;
-  logMain(`Initial fitView: ${fitView}`);
-
-  fitViewElt.addEventListener('change', function(event) {
-    fitView = event.target.checked;
-    changeZoomLevel();
-  });
-}
-
-function changeZoomLevel() {
-  logMain('Zoom level switch detected');
-  if (!loadedXml) {
-    return;
-  }
-  logMain(`Changing zoom level with fitView: ${fitView}`);
-  if (isUseBpmnVisualization()) {
-    setZoomLevelBpmnVisualization();
-  } else {
-    setZoomLevelBpmnJs();
-  }
-}
-
-function configureBpmnLibraryRadioButtons() {
-  for (let i = 0, length = bpmnLibraryRadioButtons.length; i < length; i++) {
-    bpmnLibraryRadioButtons[i].addEventListener('change', reloadAfterBpmnLibrarySwitch);
-  }
-}
-
-function isUseBpmnVisualization() {
-  return getSelectedBpmnLibrary() === 'bpmn-visualization';
-}
-
-function getSelectedBpmnLibrary() {
-  let bpmnLibrary;
-  for (let i = 0, length = bpmnLibraryRadioButtons.length; i < length; i++) {
-    if (bpmnLibraryRadioButtons[i].checked) {
-      bpmnLibrary = bpmnLibraryRadioButtons[i].value;
-      break;
-    }
-  }
-  bpmnLibrary = bpmnLibrary ?? 'bpmn-visualization';
-  logMain(`Selected lib: ${bpmnLibrary}`)
-  return bpmnLibrary;
-}
-
-
-function displayBpmnContainer(useBpmnVisualization) {
-  const bpmnVisualizationContainerElt = document.getElementById(bpmnVisualizationContainerId);
-  const bpmnJsContainerElt = document.getElementById(bpmnJsContainerId);
-
-  if (useBpmnVisualization) {
-    bpmnVisualizationContainerElt.classList.remove('hidden');
-    bpmnJsContainerElt.classList.add('hidden');
-  } else {
-    bpmnJsContainerElt.classList.remove('hidden');
-    bpmnVisualizationContainerElt.classList.add('hidden');
-  }
-}
-
-function log(header, msg) {
-  console.info(`[${header}] ${msg}`);
-}
-
-function logBpmnJs(msg) {
-  log('bpmn-js', msg)
-}
-
-function logBpmnVisu(msg) {
-  log('bpmn-visualization', msg)
-}
-
-function logMain(msg) {
-  log('main', msg)
-}
+const uiController = new UIController(state, libraryComparator, 'container-bpmn-visualization', 'container-bpmn-js');
+uiController.configure();
