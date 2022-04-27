28a9a5f83310021baecffaf4fe9f01308e1325dd
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2022-04-27 18:04:50 +0200

    [INFRA] Simplify the configuration of the TypeScript Rollup project (#323)
    Move the whole code to the `index.ts` file. We previously add an extra indirection to an `app/initialize.ts` file that
    didn't help to demonstrate the integration.
    
    Simply the commands to run the dev server: only keep the watch mode to make experiments easier. The changes are always
    reflected in the browser in development.
    
    Simplify the rollup configuration
      - remove the unnecessary json plugin
      - only keep a DEV and PRODUCTION mode
    
    Remove eslint and prettier dependencies and configuration. They added noise as they are not related to the bpmn-visualization
    integration in a Rollup project.
    
    Bump all devDependencies

--- examples/projects/typescript-vanilla-with-rollup/rollup.config.js
+++ examples/projects/typescript-vanilla-with-rollup/rollup.config.js
@@ -22,47 +22,37 @@ import typescript from "rollup-plugin-typescript2";
 import commonjs from "rollup-plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import pkg from "./package.json";
-import json from "@rollup/plugin-json";
 
-const devLiveReloadMode = process.env.devLiveReloadMode;
-const devMode = devLiveReloadMode ? true : process.env.devMode;
+const devMode = process.env.devMode ?? false;
 
 const plugins = [
-  typescript({
-    typescript: require("typescript"),
-  }),
+  typescript(),
   resolve(),
-  commonjs(),
-  json(),
+  commonjs(), // at least required to bundle mxGraph with is a CommonJS module
 ];
 
 // Copy static resources to dist
+const copyTargets = [];
+copyTargets.push({src: "src/*.html", dest: "dist/"});
+copyTargets.push({src: "src/static", dest: "dist"});
+let copyPlugin;
 if (devMode) {
-  const copyTargets = [];
-  copyTargets.push({ src: "src/*.html", dest: "dist/" });
-  copyTargets.push({ src: "src/static", dest: "dist" });
-  let copyPlugin;
-  if (devLiveReloadMode) {
-    copyPlugin = copyWatch({
-      watch: ["src/static/**", "src/*.html"],
-      targets: copyTargets,
-    });
-  } else {
-    copyPlugin = copy({
-      targets: copyTargets,
-    });
-  }
-  plugins.push(copyPlugin);
+  copyPlugin = copyWatch({
+    watch: ["src/static/**", "src/*.html"],
+    targets: copyTargets,
+  });
+} else {
+  copyPlugin = copy({
+    targets: copyTargets,
+  });
 }
+plugins.push(copyPlugin);
 
 if (devMode) {
   // Create a server for dev mode
-  plugins.push(serve({ contentBase: "dist", port: 10001 }));
-
-  if (devLiveReloadMode) {
-    // Allow to livereload on any update
-    plugins.push(livereload({ watch: "dist", verbose: true }));
-  }
+  plugins.push(serve({contentBase: "dist", port: 10001}));
+  // Allow to livereload on any update
+  plugins.push(livereload({watch: "dist", verbose: true}));
 }
 
 export default [
