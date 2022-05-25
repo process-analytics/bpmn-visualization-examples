e261467850d924076489af205074b65602241907
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2022-05-25 17:52:22 +0200

    [FEAT] Add example to customize the colors of edge markers
    In addition
      - fix the waypoints of the incident diagram: they were out of the shapes
      - fix README information in other custom colors examples about mxGraph styling
      - fix label position in a demo diagram involving edge markers

---
+++ examples/custom-bpmn-theme/custom-edge-marker-colors/README.md
@@ -0,0 +1,39 @@
+# Custom colors for Edge Markers
+
+**DISCLAIMER: this extension point is very experimental and is subject to change**.  
+
+Javascript example
+- [__⏩ live environment__](https://cdn.statically.io/gh/process-analytics/bpmn-visualization-examples/master/examples/custom-bpmn-theme/custom-edge-markers-colors/index.html)
+- to run locally, open the [index.html](index.html) directly in a Web Browser
+
+
+## ♻️ BPMN Visualization Usage
+Override the BPMN element styles using various ways. `mxGraph` knowledge is required to handle style changes.
+See the [development documentation](https://github.com/process-analytics/bpmn-visualization-js/blob/master/docs/contributors/bpmn-support-how-to.md) for more details.
+
+⚠️To avoid having to many content in the README, we simplify it. You can find all the content of the example in [index.js](index.js).
+
+ℹ The implementation proposed here extends the lib class entry point. You can also do it by [changing the default colors](../custom-colors/README.md).
+
+
+Content:
+- different fill colors for the start and end markers of the `message flows`: extend the lib class entry point
+```javascript
+class BpmnVisualizationCustomColors extends BpmnVisualization {
+
+    constructor(containerId) {
+        super({ container: containerId });
+        this.configureStyle();
+    }
+
+    configureStyle() {
+        const styleSheet = this.graph.getStylesheet(); // mxStylesheet
+
+        const style = styleSheet.styles[FlowKind.MESSAGE_FLOW];
+        style[BpmnStyleIdentifier.EDGE_START_FILL_COLOR] = '#ee1cb0';
+        style[BpmnStyleIdentifier.EDGE_END_FILL_COLOR] = 'orange';
+    }
+}
+
+const bpmnVisualizationCustomColors = new BpmnVisualizationCustomColors('bpmn-container');
+```
--- examples/custom-bpmn-theme/custom-edge-marker-colors/index.js
+++ examples/custom-bpmn-theme/custom-edge-marker-colors/index.js
@@ -1,3 +1,50 @@
+const navigation = {enabled: true};
+const loadOptions = {fit: {type: bpmnvisu.FitType.Center, margin: 20}};
+
+class BpmnVisualizationCustomizedColors extends bpmnvisu.BpmnVisualization {
+    constructor(globalOptions) {
+        super(globalOptions);
+        this.configureStyle();
+    }
+
+    configureStyle() {
+        // do nothing by default
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// custom markers for message flows
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class BpmnVisualizationCustomColorsMarkers extends BpmnVisualizationCustomizedColors {
+    configureStyle() {
+        const styleSheet = this.graph.getStylesheet(); // mxStylesheet parameter
+        const style = styleSheet.styles[bpmnvisu.FlowKind.MESSAGE_FLOW];
+        style[bpmnvisu.BpmnStyleIdentifier.EDGE_START_FILL_COLOR] = '#ee1cb0';
+        style[bpmnvisu.BpmnStyleIdentifier.EDGE_END_FILL_COLOR] = 'orange';
+    }
+}
+
+const bpmnVisualization1 = new BpmnVisualizationCustomColorsMarkers({container: 'bpmn-container-1', navigation});
+bpmnVisualization1.load(getIncidentManagementBpmnDiagram(), loadOptions);
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// custom markers for sequence flows
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class BpmnVisualizationCustomColorsMarkersSeqFlow extends BpmnVisualizationCustomizedColors {
+    configureStyle() {
+        const styleSheet = this.graph.getStylesheet(); // mxStylesheet parameter
+        const seqFlowConditionalMarkerFromActivityStyle = styleSheet.styles[bpmnvisu.SequenceFlowKind.CONDITIONAL_FROM_ACTIVITY];
+        seqFlowConditionalMarkerFromActivityStyle[bpmnvisu.BpmnStyleIdentifier.EDGE_START_FILL_COLOR] = '#6a71ee';
+    }
+}
+
+const bpmnVisualization2 = new BpmnVisualizationCustomColorsMarkersSeqFlow({container: 'bpmn-container-2', navigation});
+bpmnVisualization2.load(sequenceFlowDiagram(), loadOptions);
+
+function sequenceFlowDiagram() {
+    return `
 <?xml version="1.0" encoding="UTF-8"?>
 <bpmn:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" id="Definitions_0g0wkw3" targetNamespace="http://example.com/schema/bpmn">
   <bpmn:process id="Process_0jsrcqv" isExecutable="false">
@@ -9,24 +56,24 @@
       <bpmn:outgoing>Flow_07vj0qd</bpmn:outgoing>
       <bpmn:outgoing>Flow_1io36a6</bpmn:outgoing>
     </bpmn:task>
-    <bpmn:sequenceFlow id="Flow_0e2llvg" name="Normal Sequence flow &#10; with label Font" sourceRef="Gateway_0qsbw8c" targetRef="Activity_116eaag" />
+    <bpmn:sequenceFlow id="Flow_0e2llvg" sourceRef="Gateway_0qsbw8c" targetRef="Activity_116eaag" />
     <bpmn:exclusiveGateway id="Gateway_0hq2be7" default="Flow_0v4ibpj">
       <bpmn:incoming>Flow_0zxbqa5</bpmn:incoming>
       <bpmn:outgoing>Flow_1nstpgm</bpmn:outgoing>
       <bpmn:outgoing>Flow_0v4ibpj</bpmn:outgoing>
     </bpmn:exclusiveGateway>
-    <bpmn:sequenceFlow id="Flow_0zxbqa5" name="Normal Sequence flow" sourceRef="Gateway_0qsbw8c" targetRef="Gateway_0hq2be7" />
+    <bpmn:sequenceFlow id="Flow_0zxbqa5" sourceRef="Gateway_0qsbw8c" targetRef="Gateway_0hq2be7" />
     <bpmn:endEvent id="Event_0445f4y">
       <bpmn:incoming>Flow_06mrz3i</bpmn:incoming>
     </bpmn:endEvent>
-    <bpmn:sequenceFlow id="Flow_07vj0qd" name="Conditional Sequence flow from Activity 1" sourceRef="Activity_116eaag" targetRef="Gateway_1b07tw9">
+    <bpmn:sequenceFlow id="Flow_07vj0qd" sourceRef="Activity_116eaag" targetRef="Gateway_1b07tw9">
       <bpmn:conditionExpression xsi:type="bpmn:tFormalExpression" />
     </bpmn:sequenceFlow>
-    <bpmn:sequenceFlow id="Flow_1nstpgm" name="Conditional Sequence flow from Gateway" sourceRef="Gateway_0hq2be7" targetRef="Gateway_1b07tw9">
+    <bpmn:sequenceFlow id="Flow_1nstpgm" sourceRef="Gateway_0hq2be7" targetRef="Gateway_1b07tw9">
       <bpmn:conditionExpression xsi:type="bpmn:tFormalExpression" />
     </bpmn:sequenceFlow>
-    <bpmn:sequenceFlow id="Flow_0v4ibpj" name="Default Sequence flow" sourceRef="Gateway_0hq2be7" targetRef="Gateway_02a5re3" />
-    <bpmn:sequenceFlow id="Flow_1io36a6" name="Default Sequence flow" sourceRef="Activity_116eaag" targetRef="Gateway_02a5re3" />
+    <bpmn:sequenceFlow id="Flow_0v4ibpj" sourceRef="Gateway_0hq2be7" targetRef="Gateway_02a5re3" />
+    <bpmn:sequenceFlow id="Flow_1io36a6" sourceRef="Activity_116eaag" targetRef="Gateway_02a5re3" />
     <bpmn:parallelGateway id="Gateway_02a5re3">
       <bpmn:incoming>Flow_1io36a6</bpmn:incoming>
       <bpmn:incoming>Flow_0v4ibpj</bpmn:incoming>
@@ -46,19 +93,19 @@
       <bpmn:outgoing>Flow_0zxbqa5</bpmn:outgoing>
     </bpmn:parallelGateway>
     <bpmn:sequenceFlow id="Flow_04af1mr" sourceRef="Gateway_1b07tw9" targetRef="Activity_04acs78" />
-    <bpmn:sequenceFlow id="Flow_06mrz3i" name="Default Sequence flow" sourceRef="Activity_04acs78" targetRef="Event_0445f4y" />
-    <bpmn:sequenceFlow id="Flow_0jud8xv" name="Conditional Sequence flow from Activty 2" sourceRef="Activity_04acs78" targetRef="Event_1tkvvjj">
+    <bpmn:sequenceFlow id="Flow_06mrz3i" sourceRef="Activity_04acs78" targetRef="Event_0445f4y" />
+    <bpmn:sequenceFlow id="Flow_0jud8xv" sourceRef="Activity_04acs78" targetRef="Event_1tkvvjj">
       <bpmn:conditionExpression xsi:type="bpmn:tFormalExpression" />
     </bpmn:sequenceFlow>
-    <bpmn:endEvent id="Event_1tkvvjj">
-      <bpmn:incoming>Flow_0jud8xv</bpmn:incoming>
-      <bpmn:messageEventDefinition id="MessageEventDefinition_0k2ly7l" />
-    </bpmn:endEvent>
     <bpmn:task id="Activity_04acs78" default="Flow_06mrz3i">
       <bpmn:incoming>Flow_04af1mr</bpmn:incoming>
       <bpmn:outgoing>Flow_06mrz3i</bpmn:outgoing>
       <bpmn:outgoing>Flow_0jud8xv</bpmn:outgoing>
     </bpmn:task>
+    <bpmn:endEvent id="Event_1tkvvjj">
+      <bpmn:incoming>Flow_0jud8xv</bpmn:incoming>
+      <bpmn:signalEventDefinition id="SignalEventDefinition_03inplf" />
+    </bpmn:endEvent>
   </bpmn:process>
   <bpmndi:BPMNDiagram id="BPMNDiagram_1">
     <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_0jsrcqv">
@@ -66,16 +113,10 @@
         <di:waypoint x="850" y="300" />
         <di:waypoint x="850" y="440" />
         <di:waypoint x="1032" y="440" />
-        <bpmndi:BPMNLabel labelStyle="BPMNLabelStyle_1">
-          <dc:Bounds x="853" y="330" width="73" height="40" />
-        </bpmndi:BPMNLabel>
       </bpmndi:BPMNEdge>
       <bpmndi:BPMNEdge id="Flow_06mrz3i_di" bpmnElement="Flow_06mrz3i">
         <di:waypoint x="900" y="260" />
         <di:waypoint x="1032" y="260" />
-        <bpmndi:BPMNLabel>
-          <dc:Bounds x="926" y="226" width="88" height="27" />
-        </bpmndi:BPMNLabel>
       </bpmndi:BPMNEdge>
       <bpmndi:BPMNEdge id="Flow_04af1mr_di" bpmnElement="Flow_04af1mr">
         <di:waypoint x="755" y="260" />
@@ -92,48 +133,30 @@
       <bpmndi:BPMNEdge id="Flow_1io36a6_di" bpmnElement="Flow_1io36a6">
         <di:waypoint x="490" y="139" />
         <di:waypoint x="490" y="235" />
-        <bpmndi:BPMNLabel>
-          <dc:Bounds x="396" y="171.00000000000023" width="88" height="27" />
-        </bpmndi:BPMNLabel>
       </bpmndi:BPMNEdge>
       <bpmndi:BPMNEdge id="Flow_0v4ibpj_di" bpmnElement="Flow_0v4ibpj">
         <di:waypoint x="490" y="415" />
         <di:waypoint x="490" y="285" />
-        <bpmndi:BPMNLabel>
-          <dc:Bounds x="396" y="356" width="88" height="27" />
-        </bpmndi:BPMNLabel>
       </bpmndi:BPMNEdge>
       <bpmndi:BPMNEdge id="Flow_1nstpgm_di" bpmnElement="Flow_1nstpgm">
         <di:waypoint x="515" y="440" />
         <di:waypoint x="730" y="440" />
         <di:waypoint x="730" y="285" />
-        <bpmndi:BPMNLabel>
-          <dc:Bounds x="563" y="390" width="73" height="40" />
-        </bpmndi:BPMNLabel>
       </bpmndi:BPMNEdge>
       <bpmndi:BPMNEdge id="Flow_07vj0qd_di" bpmnElement="Flow_07vj0qd">
         <di:waypoint x="540" y="99" />
         <di:waypoint x="730" y="99" />
         <di:waypoint x="730" y="235" />
-        <bpmndi:BPMNLabel>
-          <dc:Bounds x="556" y="106" width="73" height="40" />
-        </bpmndi:BPMNLabel>
       </bpmndi:BPMNEdge>
       <bpmndi:BPMNEdge id="Flow_0zxbqa5_di" bpmnElement="Flow_0zxbqa5">
         <di:waypoint x="270" y="285" />
         <di:waypoint x="270" y="440" />
         <di:waypoint x="465" y="440" />
-        <bpmndi:BPMNLabel>
-          <dc:Bounds x="275" y="456" width="89" height="27" />
-        </bpmndi:BPMNLabel>
       </bpmndi:BPMNEdge>
       <bpmndi:BPMNEdge id="Flow_0e2llvg_di" bpmnElement="Flow_0e2llvg">
         <di:waypoint x="270" y="235" />
         <di:waypoint x="270" y="99" />
         <di:waypoint x="440" y="99" />
-        <bpmndi:BPMNLabel labelStyle="BPMNLabelStyle_2">
-          <dc:Bounds x="175" y="100" width="89" height="40" />
-        </bpmndi:BPMNLabel>
       </bpmndi:BPMNEdge>
       <bpmndi:BPMNShape id="_BPMNShape_StartEvent_2" bpmnElement="StartEvent_1omufvy">
         <dc:Bounds x="156" y="242" width="36" height="36" />
@@ -156,18 +179,14 @@
       <bpmndi:BPMNShape id="Gateway_0eacrzg_di" bpmnElement="Gateway_0qsbw8c">
         <dc:Bounds x="245" y="235" width="50" height="50" />
       </bpmndi:BPMNShape>
-      <bpmndi:BPMNShape id="Event_16z3xs7_di" bpmnElement="Event_1tkvvjj">
-        <dc:Bounds x="1032" y="422" width="36" height="36" />
-      </bpmndi:BPMNShape>
       <bpmndi:BPMNShape id="Activity_11qmyek_di" bpmnElement="Activity_04acs78">
         <dc:Bounds x="800" y="220" width="100" height="80" />
       </bpmndi:BPMNShape>
+      <bpmndi:BPMNShape id="Event_04oou7y_di" bpmnElement="Event_1tkvvjj">
+        <dc:Bounds x="1032" y="422" width="36" height="36" />
+      </bpmndi:BPMNShape>
     </bpmndi:BPMNPlane>
-    <bpmndi:BPMNLabelStyle id="BPMNLabelStyle_1">
-      <dc:Font name="Ubuntu" size="9" isBold="true" isItalic="true" isUnderline="true" isStrikeThrough="false" />
-    </bpmndi:BPMNLabelStyle>
-    <bpmndi:BPMNLabelStyle id="BPMNLabelStyle_2">
-      <dc:Font name="Segoe UI" size="14" isBold="false" isItalic="true" isUnderline="false" isStrikeThrough="true" />
-    </bpmndi:BPMNLabelStyle>
   </bpmndi:BPMNDiagram>
 </bpmn:definitions>
+    `;
+}
