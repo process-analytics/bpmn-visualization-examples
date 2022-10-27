58e16f07d06819ac851c257c2e44dc54c1ae61dd
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2022-10-27 17:42:20 +0200

    fix: don't load on fit changes, fit only

--- projects/typescript-angular/bpmn-visualization-app/src/app/bpmn-visualization/bpmn-visualization.component.ts
+++ projects/typescript-angular/bpmn-visualization-app/src/app/bpmn-visualization/bpmn-visualization.component.ts
@@ -1,5 +1,5 @@
-import {Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild,} from '@angular/core';
-import {BpmnVisualization, FitType} from 'bpmn-visualization';
+import {Component, type ElementRef, Input, type OnChanges, type OnInit, type SimpleChanges, ViewChild,} from '@angular/core';
+import {BpmnVisualization, type FitOptions, FitType} from 'bpmn-visualization';
 
 @Component({
   selector: 'app-bpmn-visualization',
@@ -21,28 +21,48 @@ export class BpmnVisualizationComponent implements OnInit, OnChanges {
   constructor() {}
 
   ngOnInit(): void {
+    console.info('calling ngOnInit')
     this.bpmnVisualization = new BpmnVisualization({
       container: this.bpmnContainer!.nativeElement,
       navigation: { enabled: true },
     });
 
+    // TODO why do we need to call this here? if not the first display is not done
+    console.info('calling loadDiagram from ngOnInit')
     this.loadDiagram();
   }
 
   ngOnChanges(_changes: SimpleChanges): void {
-    this.loadDiagram();
+    // console.info('changes', _changes);
+    if (_changes['diagram']) {
+      console.info('detected diagram changes, call loadDiagram');
+      this.loadDiagram();
+    } else if (_changes['fitType']) {
+      console.info('detected fit changes');
+      this.fit();
+    }
   }
 
-  // TODO diagram load is called every time a changes occur
   private loadDiagram(): void {
     if (this.diagram) {
       loadCall++;
       console.info('@@load diagram', loadCall);
       this.bpmnVisualization?.load(this.diagram, {
-        fit: { type: this.fitType, margin: this.fitType != FitType.None? 20: 0 },
+        fit: fitOptions(this.fitType),
       });
     }
   }
+
+  private fit(): void {
+    fitCall++;
+    console.info('call fit', fitCall);
+    this.bpmnVisualization?.navigation.fit(fitOptions(this.fitType));
+  }
+}
+
+const fitOptions = (type: FitType | undefined): FitOptions => {
+  return {type: type, margin: type != FitType.None? 20: 0};
 }
 
 let loadCall = 0;
+let fitCall = 0;
