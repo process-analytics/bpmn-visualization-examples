4a83feef37a9081db7a3a63de5ab23f13a62cf44
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2022-11-02 06:58:53 +0100

    component: clean console log

--- projects/typescript-angular/bpmn-visualization-app/src/app/bpmn-visualization/bpmn-visualization.component.ts
+++ projects/typescript-angular/bpmn-visualization-app/src/app/bpmn-visualization/bpmn-visualization.component.ts
@@ -5,7 +5,6 @@ import {
   type ElementRef,
   Input,
   type OnChanges,
-  type OnInit,
   type SimpleChanges,
   ViewChild,
 } from '@angular/core';
@@ -16,9 +15,8 @@ import {BpmnVisualization, type FitOptions, FitType} from 'bpmn-visualization';
   templateUrl: './bpmn-visualization.component.html',
   styleUrls: ['./bpmn-visualization.component.css'],
 })
-export class BpmnVisualizationComponent implements OnInit, OnChanges, AfterContentInit, AfterViewInit {
+export class BpmnVisualizationComponent implements OnChanges, AfterViewInit {
   @Input()
-  // diagram!: string | null;
   diagram?: string;
 
   @Input()
@@ -29,21 +27,8 @@ export class BpmnVisualizationComponent implements OnInit, OnChanges, AfterConte
 
   private bpmnVisualization: BpmnVisualization | undefined;
 
-  constructor() {
-    console.info('calling constructor')
-    console.info('bpmnContainer', this.bpmnContainer);
-  }
-
-  ngOnInit(): void {
-    this.logLifeCycleStep('ngOnInit')
-  }
-
-  // TODO need to hook to init bpmn-visu AfterContentInit
-  // --> switch to AfterViewInit ngAfterViewInit?
-  ngAfterContentInit(): void {
-    this.logLifeCycleStep('ngAfterContentInit')
+  ngAfterViewInit(): void {
     if (!this.bpmnVisualization) {
-      console.info('Init the BpmnVisualization instance')
       // initialize BpmnVisualization instance with the component DOM element
       this.bpmnVisualization = new BpmnVisualization({
         container: this.bpmnContainer!.nativeElement,
@@ -52,32 +37,17 @@ export class BpmnVisualizationComponent implements OnInit, OnChanges, AfterConte
     }
   }
 
-  ngAfterViewInit(): void {
-    this.logLifeCycleStep('ngAfterViewInit')
-  }
-
   ngOnChanges(_changes: SimpleChanges): void {
-    this.logLifeCycleStep('ngOnChanges')
-    console.info('changes', _changes);
     if (_changes['diagram'] && _changes['diagram'].currentValue) {
-      console.info('detected diagram changes, call loadDiagram');
       this.loadDiagram();
     } else if (_changes['fitType'] && _changes['fitType'].currentValue && this.diagram) {
       // only fit when a diagram has been loaded
-      console.info('detected fit changes');
       this.fit();
     }
   }
 
-  private logLifeCycleStep(step: string): void {
-    console.info('calling', step)
-    console.info('bpmnContainer', this.bpmnContainer);
-  }
-
   private loadDiagram(): void {
     if (this.diagram) { // check to make TS happy, be we call load diagram only when the diagram is set
-      loadCall++;
-      console.info('@@load diagram', loadCall);
       this.bpmnVisualization?.load(this.diagram, {
         fit: fitOptions(this.fitType),
       });
@@ -85,8 +55,6 @@ export class BpmnVisualizationComponent implements OnInit, OnChanges, AfterConte
   }
 
   private fit(): void {
-    fitCall++;
-    console.info('call fit', fitCall);
     this.bpmnVisualization?.navigation.fit(fitOptions(this.fitType));
   }
 }
@@ -94,6 +62,3 @@ export class BpmnVisualizationComponent implements OnInit, OnChanges, AfterConte
 const fitOptions = (type: FitType | undefined): FitOptions => {
   return {type: type, margin: type != FitType.None? 20: 0};
 }
-
-let loadCall = 0;
-let fitCall = 0;
