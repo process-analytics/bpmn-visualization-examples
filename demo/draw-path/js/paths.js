73009f33d404a7a262f34fc66a2da62f6f346b71
Souchet Céline <celine.souchet@bonitasoft.com>
2023-03-01 10:57:07 +0100

    refactor(demo): use the new flow/flownode links from the BPMN Semantic in the draw-path demo

--- demo/draw-path/js/paths.js
+++ demo/draw-path/js/paths.js
@@ -22,30 +22,25 @@ class Path {
     }
 }
 
-const paths = [
-    new Path('start_event', "sequence_flow_1", "parallel_gateway_1"),
-    new Path('parallel_gateway_1', "sequence_flow_2", "task_1"),
-    new Path('parallel_gateway_1', "sequence_flow_18", "task_2"),
-    new Path('task_1', "sequence_flow_3", "exclusive_gateway_1"),
-    new Path('task_2', "sequence_flow_15", "parallel_gateway_2"),
-    new Path('exclusive_gateway_1', "sequence_flow_4", "task_3"),
-    new Path('exclusive_gateway_1', "sequence_flow_5", "task_5"),
-    new Path('exclusive_gateway_2', "sequence_flow_14", "parallel_gateway_2"),
-    new Path('parallel_gateway_2', "sequence_flow_16", "task_8"),
-    new Path('task_8', "sequence_flow_17", "end_event"),
-    new Path('task_3', "sequence_flow_12", "task_4"),
-    new Path('task_4', "sequence_flow_13", "exclusive_gateway_2"),
-    new Path('task_5', "sequence_flow_6", "inclusive_gateway_1"),
-    new Path('inclusive_gateway_1', "sequence_flow_7", "task_7"),
-    new Path('inclusive_gateway_1', "sequence_flow_8", "task_6"),
-    new Path('inclusive_gateway_2', "sequence_flow_11", "exclusive_gateway_2"),
-    new Path('task_7', "sequence_flow_10", "inclusive_gateway_2"),
-    new Path('task_6', "sequence_flow_9", "inclusive_gateway_2")
-];
+class PathManager {
 
-const doActionOnPath = (filter, actionOnFilteredPath) => {
-    const filteredPaths = paths.filter(path => filter(path));
-    if (filteredPaths.length > 0) {
-        actionOnFilteredPath(filteredPaths[0]);
+    #paths;
+
+    constructor(edges) {
+        this.#paths = this.#buildPaths(edges);
+    }
+
+    doActionOnPath(filter, actionOnFilteredPath)  {
+        const filteredPaths = this.#paths.filter(path => filter(path));
+        if (filteredPaths.length > 0) {
+            actionOnFilteredPath(filteredPaths[0]);
+        }
+    }
+
+    #buildPaths(edges) {
+        return edges.map(edge => {
+            const bpmnSemantic = edge.bpmnSemantic;
+            return new Path(bpmnSemantic.sourceRefId, bpmnSemantic.id , bpmnSemantic.targetRefId);
+        });
     }
 }
