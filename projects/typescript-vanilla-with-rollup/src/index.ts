7b640e27ca57adb7b6f0c55a946a8048a44ff843
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-04-26 16:05:37 +0200

    rollup

--- projects/typescript-vanilla-with-rollup/src/index.ts
+++ projects/typescript-vanilla-with-rollup/src/index.ts
@@ -24,6 +24,61 @@ const bpmnVisualization = new BpmnVisualization({
 });
 // load the BPMN diagram defined above
 bpmnVisualization.load(diagram);
+const registry = bpmnVisualization.bpmnElementsRegistry;
+
+// Style elements with CSS
+registry.addCssClasses(
+  [
+      'Activity_1t65hvk', // Create Purchase Order Item
+      'Activity_00vbm9s', // Record Goods Receipts
+  ],
+  'bpmn-highlight',
+);
+
+// Style elements with the "Update Style" API
+registry.updateStyle(['Gateway_1ezcj46', 'Activity_0yabbur', 'Event_07598zy'],
+  {
+      stroke: { color: 'blue', width: 6 },
+      fill: { color: 'orange', opacity: 40 },
+  });
+registry.updateStyle(['Flow_1kkicvr', 'Flow_12q12yb'],
+  {
+      stroke: { color: 'blue', width: 4 },
+  });
+
+// Add overlays
+// Record Invoice Receipt
+registry.addOverlays('Activity_1u4jwkv',
+  {
+      label: '150',
+      position: 'top-center',
+      style: {
+          font: {
+              size: 18
+          },
+          stroke: {
+              color: 'white'
+          }
+      },
+  });
+// Remove Payment Block
+registry.addOverlays('Activity_083jf01',
+  {
+      label: '72',
+      position: 'top-center',
+      style: {
+          fill: {
+              color: '#ff0101'
+          },
+          font: {
+              color: 'white',
+              size: 22,
+          },
+          stroke: {
+              color: '#ff0101',
+          },
+      },
+  });
 
 // display the bpmn-visualization version in the footer
 const footer = document.querySelector<HTMLElement>("footer")!;
