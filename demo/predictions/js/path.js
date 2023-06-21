f90c9493e44c208206f2efde6b279d5a8e70314b
Souchet Céline <celine.souchet@bonitasoft.com>
2023-06-21 15:55:04 +0200

    Build the paths from the shape ids

---
+++ demo/predictions/js/path.js
@@ -0,0 +1,39 @@
+// From Bonita Day 2023 Demo
+class PathResolver {
+    private bpmnVisualization;
+
+    constructor(bpmnVisualization) {
+        this.bpmnVisualization = bpmnVisualization;
+    }
+
+    getVisitedEdges(shapeIds) {
+        const edgeIds = new Set<string>();
+        for (const shapeId of shapeIds) {
+            const shapeElt = this.bpmnVisualization.bpmnElementsRegistry.getElementsByIds(shapeId)[0];
+            if (!shapeElt) {
+                continue;
+            }
+
+            const bpmnSemantic = shapeElt.bpmnSemantic;
+            const incomingEdges = bpmnSemantic.incomingIds;
+            const outgoingEdges = bpmnSemantic.outgoingIds;
+            for (const edgeId of incomingEdges) {
+                const edgeElement = this.bpmnVisualization.bpmnElementsRegistry.getElementsByIds(edgeId)[0];
+                const sourceRef = edgeElement.bpmnSemantic.sourceRefId;
+                if (shapeIds.includes(sourceRef)) {
+                    edgeIds.add(edgeId);
+                }
+            }
+
+            for (const edgeId of outgoingEdges) {
+                const edgeElement = this.bpmnVisualization.bpmnElementsRegistry.getElementsByIds(edgeId)[0];
+                const targetRef = edgeElement.bpmnSemantic.targetRefId;
+                if (shapeIds.includes(targetRef)) {
+                    edgeIds.add(edgeId);
+                }
+            }
+        }
+
+        return Array.from(edgeIds);
+    }
+}
