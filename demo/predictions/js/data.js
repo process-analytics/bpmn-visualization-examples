79fdea324cad16dbacc0b747ab1f9cc5c1c907d9
Souchet Céline <celine.souchet@bonitasoft.com>
2023-06-21 12:26:05 +0200

    introduce data execution managers

--- demo/predictions/js/data.js
+++ demo/predictions/js/data.js
@@ -1,77 +1,126 @@
-const predicatedLateAlreadyExecutedElements = alreadyExecutedElementsPredictedLate;
-const predicatedLateData = {
-    elementWithPrediction: {
-        bpmnId: vendorBakeThePizzaId,
-        cssClasses: 'state-predicted-late',
-    },
-    predictedPath: {
-        ids: [
-            // customer elements
-            customerEvtBasedGwId,
-            '_6-424', // sequence flow between 'event-based gateway' (running element) and timer event
-            '_6-219', // timer event
-            '_6-426', // sequence flow between 'timer event' and 'Ask for the pizza'
-            '_6-236', // 'Ask for the pizza'
-            // message flow
-            '_6-642',
-            // vendor elements
-            vendorWhereIsMyPizzaId,
-            '_6-748', // sequence flow between 'where is my pizza' and 'Calm customer'
-            '_6-695', // 'Calm customer'
-        ],
-        cssClasses: 'path-predicted-late',
-    },
-};
+class DataExecutionManager {
 
+    private _runningElementsWithoutPrediction = ['_6-674' // vendor where is my pizza
+        , '_6-180' // customer evt based gateway
+    ];
 
-const alreadyExecutedElementsPredictedLate = [
-    // customer
-    '_6-61', // start event
-    '_6-125', // sequence flow between 'start event' and 'select pizza'
-    '_6-74', // select a pizza
-    '_6-178', // sequence flow between 'select a pizza' and 'order a pizza'
-    '_6-127', // order a pizza
-    '_6-420', // sequence flow between 'order a pizza' and 'event-based gateway' (running element)
+    _alreadyExecutedElementsPredictedLate = [
+        // customer
+        '_6-61', // start event
+        '_6-125', // sequence flow between 'start event' and 'select pizza'
+        '_6-74', // select a pizza
+        '_6-178', // sequence flow between 'select a pizza' and 'order a pizza'
+        '_6-127', // order a pizza
+        '_6-420', // sequence flow between 'order a pizza' and 'event-based gateway' (running element)
 
-    '_6-638', // message flow
+        '_6-638', // message flow
 
-    // vendor
-    '_6-450', // order received
-    '_6-630', // sequence flow between 'order received' and 'parallel gateway'
-    '_6-652', // parallel gateway
-    '_6-693', // sequence flow between 'parallel gateway' and 'Bake the pizza'
-    '_6-691', // sequence flow between 'parallel gateway' and 'where is my pizza'
-];
+        // vendor
+        '_6-450', // order received
+        '_6-630', // sequence flow between 'order received' and 'parallel gateway'
+        '_6-652', // parallel gateway
+        '_6-693', // sequence flow between 'parallel gateway' and 'Bake the pizza'
+        '_6-691', // sequence flow between 'parallel gateway' and 'where is my pizza'
+    ];
 
-const vendorBakeThePizzaId = '_6-463';
-const vendorWhereIsMyPizzaId = '_6-674';
-const customerEvtBasedGwId = '_6-180';
-
-const predictedOnTimeData = {
-    elementWithPrediction: {
-        bpmnId: '_6-514', // vendor 'Deliver the pizza'
-        cssClasses: 'state-predicted-on-time',
-    },
-    predictedPath: {
-        ids: [
-            // customer elements
-            customerEvtBasedGwId,
-            '_6-422', // sequence flow between 'event-based gateway' (running element) and msg event
-            '_6-202', // msg event 'Pizza received'
-            '_6-428', // sequence flow between 'msg event' and 'Pay the pizza'
-            '_6-304', // 'Pay the pizza'
-            // message flow
-            '_6-640', // from vendor 'Deliver the pizza' to customer 'msg event'
-            // vendor elements
-            '_6-634', // sequence flow between 'Deliver the pizza' and 'Receive payment'
-            '_6-565', // 'Receive payment'
-        ],
-        cssClasses: 'path-predicted-on-time'
+    get executedElements() {
+        return [];
     }
-};
 
-predictedOnTimeAlreadyExecutedElements = [...alreadyExecutedElementsPredictedLate, ...[
-    // vendor
-    vendorBakeThePizzaId,
-    '_6-632', // sequence flow between 'Bake the pizza' and 'Deliver the pizza'
-]];
+    get elementWithPrediction() {
+        return null;
+    }
+
+    get predictedPath() {
+        return null;
+    }
+
+    get runningElementsWithoutPrediction() {
+        return this._runningElementsWithoutPrediction;
+    }
+}
+
+class PredicatedLateDataExecutionManager extends DataExecutionManager {
+
+    private data = {
+        elementWithPrediction: {
+            bpmnId: vendorBakeThePizzaId,
+            cssClasses: 'state-predicted-late',
+        },
+        predictedPath: {
+            ids: [
+                // customer elements
+                customerEvtBasedGwId,
+                '_6-424', // sequence flow between 'event-based gateway' (running element) and timer event
+                '_6-219', // timer event
+                '_6-426', // sequence flow between 'timer event' and 'Ask for the pizza'
+                '_6-236', // 'Ask for the pizza'
+                // message flow
+                '_6-642',
+                // vendor elements
+                vendorWhereIsMyPizzaId,
+                '_6-748', // sequence flow between 'where is my pizza' and 'Calm customer'
+                '_6-695', // 'Calm customer'
+            ],
+            cssClasses: 'path-predicted-late',
+        },
+    };
+
+    get executedElements() {
+        return this._alreadyExecutedElementsPredictedLate;
+    }
+
+    get elementWithPrediction() {
+        return this.data.elementWithPrediction;
+    }
+
+    get predictedPath() {
+        return this.data.predictedPath;
+    }
+
+}
+
+class PredictedOnTimeDataExecutionManager extends DataExecutionManager {
+
+    private data = {
+        elementWithPrediction: {
+            bpmnId: '_6-514', // vendor 'Deliver the pizza'
+            cssClasses: 'state-predicted-on-time',
+        },
+        predictedPath: {
+            ids: [
+                // customer elements
+                customerEvtBasedGwId,
+                '_6-422', // sequence flow between 'event-based gateway' (running element) and msg event
+                '_6-202', // msg event 'Pizza received'
+                '_6-428', // sequence flow between 'msg event' and 'Pay the pizza'
+                '_6-304', // 'Pay the pizza'
+                // message flow
+                '_6-640', // from vendor 'Deliver the pizza' to customer 'msg event'
+                // vendor elements
+                '_6-634', // sequence flow between 'Deliver the pizza' and 'Receive payment'
+                '_6-565', // 'Receive payment'
+            ],
+            cssClasses: 'path-predicted-on-time'
+        }
+    };
+
+    private alreadyExecutedElements = [...this._alreadyExecutedElementsPredictedLate, ...[
+        // vendor
+        '_6-463', // bake the pizza
+        '_6-632', // sequence flow between 'Bake the pizza' and 'Deliver the pizza'
+    ]];
+
+    get executedElements() {
+        return this.alreadyExecutedElements;
+    }
+
+    get elementWithPrediction() {
+        return this.data.elementWithPrediction;
+    }
+
+    get predictedPath() {
+        return this.data.predictedPath;
+    }
+
+}
--- demo/predictions/js/prediction-use-case.js
+++ demo/predictions/js/prediction-use-case.js
@@ -1,6 +1,7 @@
 class PredicatedLateUseCase extends UseCase {
     constructor(type) {
         super(type, () => pizzaDiagram(), true, {fit: {type: 'Center', margin: 20}});
+        this._dataExecutionManager = new PredicatedLateDataExecutionManager();
     }
 
     _postLoadDiagram() {
@@ -11,16 +12,17 @@ class PredicatedLateUseCase extends UseCase {
     }
 
     _reduceVisibilityOfAlreadyExecutedElements() {
-        this._bpmnVisualization.bpmnElementsRegistry.addCssClasses(this._getExecutedElements(), 'state-already-executed');
+        this._bpmnVisualization.bpmnElementsRegistry.addCssClasses(this._dataExecutionManager.executedElements(), 'state-already-executed');
     }
 
     _registerInteractions() {
         // on hover, highlight the predicted path
-        const elementTogglingPath = this._bpmnVisualization.bpmnElementsRegistry.getElementsByIds(this._getElementWithPrediction().bpmnId)[0]; // exist and only one
+        const elementTogglingPath = this._bpmnVisualization.bpmnElementsRegistry.getElementsByIds(this._dataExecutionManager.elementWithPrediction().bpmnId)[0]; // exist and only one
 
         const highlightPredictedPath = () => {
             this._toggleHighlightRunningElementsWithoutPrediction();
-            this._bpmnVisualization.bpmnElementsRegistry.toggleCssClasses(this._getPredictedPath().ids, this._getPredictedPath().cssClasses);
+            let predictedPath = this._dataExecutionManager.predictedPath();
+            this._bpmnVisualization.bpmnElementsRegistry.toggleCssClasses(predictedPath.ids, predictedPath.cssClasses);
         }
 
         elementTogglingPath.htmlElement.addEventListener('mouseenter', highlightPredictedPath);
@@ -28,29 +30,12 @@ class PredicatedLateUseCase extends UseCase {
     }
 
     _toggleHighlightRunningElementsWithoutPrediction() {
-        this._bpmnVisualization.bpmnElementsRegistry.toggleCssClasses(this._getRunningElementsWithoutPrediction(), 'state-running');
+        this._bpmnVisualization.bpmnElementsRegistry.toggleCssClasses(this._dataExecutionManager.runningElementsWithoutPrediction(), 'state-running');
     }
 
     _highlightRunningElementsWithPrediction() {
-        const elementWithPrediction = this._getElementWithPrediction();
+        const elementWithPrediction =  this._dataExecutionManager.elementWithPrediction();
         this._bpmnVisualization.bpmnElementsRegistry.addCssClasses(elementWithPrediction.bpmnId, elementWithPrediction.cssClasses);
-    }
-
-
-     _getExecutedElements() {
-        return predicatedLateAlreadyExecutedElements;
-    }
-
-    _getElementWithPrediction() {
-        return predicatedLateData.elementWithPrediction;
-    }
-
-    _getPredictedPath() {
-        return predicatedLateData.predictedPath;
-    }
-
-    _getRunningElementsWithoutPrediction() {
-        return [vendorWhereIsMyPizzaId, customerEvtBasedGwId];
     }
 }
 
@@ -58,17 +43,6 @@ class PredicatedLateUseCase extends UseCase {
 class PredictedOnTimeUseCase extends PredicatedLateUseCase {
     constructor(type) {
         super(type);
-    }
-
-    _getExecutedElements() {
-        return predictedOnTimeAlreadyExecutedElements;
-    }
-
-    _getElementWithPrediction() {
-        return predictedOnTimeData.elementWithPrediction;
-    }
-
-    _getPredictedPath() {
-        return predictedOnTimeData.predictedPath;
+        this._dataExecutionManager = new PredictedOnTimeDataExecutionManager();
     }
 }
