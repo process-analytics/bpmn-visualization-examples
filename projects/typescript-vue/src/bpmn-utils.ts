6c79e20f05e65d6b64b4aa9f19d6779e12481f5c
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-07-24 11:47:50 +0200

    refactor(vue-demo): use `bv-experimental-add-ons`
    Use the new `ShapeUtil.isBpmnArtifact` method instead of the custom implementation stored in the demo.

--- projects/typescript-vue/src/bpmn-utils.ts
+++
@@ -1,6 +0,0 @@
-import { ShapeBpmnElementKind } from 'bpmn-visualization';
-
-export const isBpmnArtifact = (kind: ShapeBpmnElementKind): boolean => {
-  // may be directly available in bpmn-visualization in the future
-  return kind === ShapeBpmnElementKind.GROUP || kind === ShapeBpmnElementKind.TEXT_ANNOTATION ;
-};
