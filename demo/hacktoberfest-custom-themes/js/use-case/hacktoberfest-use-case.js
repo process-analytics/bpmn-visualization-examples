b79d2c57235b97fac794e565700de1f1bf9ebf26
Souchet Céline <celine.souchet@bonitasoft.com>
2023-09-26 17:00:18 +0200

    Simplify how to set theme on Hacktoberfest demo

--- demo/hacktoberfest-custom-themes/js/use-case/hacktoberfest-use-case.js
+++ demo/hacktoberfest-custom-themes/js/use-case/hacktoberfest-use-case.js
@@ -8,6 +8,11 @@ class HacktoberfestUseCase extends UseCase {
         });
     }
 
+    _initBpmnVisualization(options) {
+        super._initBpmnVisualization();
+        bpmnvisu.IconPainterProvider.set(new bpmnvisu.IconPainter());
+    }
+
     updateCellsLabel(projectName) {
         this._updateCellLabel("call_activity", `Contribute to ${projectName} 🔧`);
         this._updateCellLabel("user_task_5", `Tweet how it was fun to contribute to ${projectName} 😃`);
@@ -19,13 +24,6 @@ class HacktoberfestUseCase extends UseCase {
     _updateCellLabel(cellId, value) {
         const cell = this._bpmnVisualization.graph.model.getCell(cellId);
         this._bpmnVisualization.graph.model.setValue(cell, value);
-    }
-
-    /**
-     * Generic implementation
-     */
-    _preLoadDiagram() {
-        bpmnvisu.IconPainterProvider.set(new bpmnvisu.IconPainter());
     }
 
 }
