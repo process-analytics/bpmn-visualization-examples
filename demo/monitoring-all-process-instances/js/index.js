e862fce9e0e6c0b229ca36fb1bd605387cb102fb
Souchet Céline <4921914+csouchet@users.noreply.github.com>
2023-10-02 16:58:46 +0200

    refactor(demo): use a single bpmn container in the monitoring demo (#559)

--- demo/monitoring-all-process-instances/js/index.js
+++ demo/monitoring-all-process-instances/js/index.js
@@ -1,36 +1,39 @@
-// Initialize UseCases
-const timeUseCase = new MonitoringUseCase('time', getHardwareRetailerDiagram, new TimeExecutionData());
-const frequencyUseCase = new MonitoringUseCase('frequency', getHardwareRetailerDiagram, new FrequencyExecutionData());
+function buildUseCase(type) {
+    switch (type) {
+        case 'time':
+            return  new MonitoringUseCase(  getHardwareRetailerDiagram, new TimeExecutionData(), "Average run time");
+        case 'frequency':
+        default:
+            return new MonitoringUseCase(  getHardwareRetailerDiagram, new FrequencyExecutionData(), "Number of execution");
+    }
+}
 
-/**
- * @param {string} useCaseType
- * @returns {MonitoringUseCase}
- */
-const detectActualUseCase = useCaseType => useCaseType === 'frequency' ? frequencyUseCase : timeUseCase
+function changeUseCase() {
+    state.useCase = buildUseCase(state.useCaseType);
+    state.useCase.display(state.dataType);
+}
 
 // Initialize the state and the radio buttons
 const parameters = new URLSearchParams(window.location.search);
 const state = {
-    useCase: detectActualUseCase(['frequency', 'time'].includes(parameters.get('useCase')) ? parameters.get('useCase') : 'time'),
+    useCase: undefined,
+    useCaseType: ['frequency', 'time'].includes(parameters.get('useCase')) ? parameters.get('useCase') : 'time',
     dataType: ['both', 'overlays', 'paths'].includes(parameters.get('dataType')) ? parameters.get('dataType') : 'both'
 }
-document.getElementById(`btn-${state.useCase.type}`).checked = true;
+document.getElementById(`btn-${state.useCaseType}`).checked = true;
 document.getElementById(`btn-${state.dataType}`).checked = true;
 
 document.addEventListener('DOMContentLoaded', function () {
     // Waiting for the displayed page before to load diagram & display data
-    state.useCase.display(state.dataType);
+    changeUseCase();
 })
 
 document.getElementById('choose-use-case-panel').onchange = () => {
-    const useCaseType = document.querySelector("input[type='radio'][name='use-case-type']:checked").value;
-    state.useCase = detectActualUseCase(useCaseType);
-
-    state.useCase.display(state.dataType);
+    state.useCaseType = document.querySelector("input[type='radio'][name='use-case-type']:checked").value;
+    changeUseCase();
 }
 
 document.getElementById('choose-data-panel').onchange = () => {
     state.dataType = document.querySelector("input[type='radio'][name='data-type']:checked").value;
-
-    state.useCase.displayData(state.dataType);
+    changeUseCase();
 }
