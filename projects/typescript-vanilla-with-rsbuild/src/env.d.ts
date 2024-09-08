e502a75d67498114110ef8c3fe2e953f2a5dd123
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2024-09-08 18:06:01 +0200

    Remove temp types used during the investigation of the surge.sh issue

--- projects/typescript-vanilla-with-rsbuild/src/env.d.ts
+++ projects/typescript-vanilla-with-rsbuild/src/env.d.ts
@@ -2,14 +2,3 @@ declare module '*.xml' {
   const content: string;
   export default content;
 }
-
-// TMP - to investigate surge.sh issue
-declare module '*.txt' {
-  const content: string;
-  export default content;
-}
-// declare module '*.xml' {
-//   const content: string;
-//   export default content;
-// }
-// end of TMP
